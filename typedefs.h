027c0e46ee89f34d6c36e4ffe0fa13b8b56b9b69
Dennis Luxen <luxen@kit.edu>
2010-08-25 19:15:17 +0000

    Compilation fixes for Mac. Works with g++ 4.4 from MacPorts

--- typedefs.h
+++ typedefs.h
@@ -21,6 +21,10 @@ or see http://www.gnu.org/licenses/agpl.txt.
 #ifndef TYPEDEFS_H_
 #define TYPEDEFS_H_
 
+#ifdef __APPLE__
+#include <signal.h>
+#endif
+
 #include <climits>
 
 using namespace std;
