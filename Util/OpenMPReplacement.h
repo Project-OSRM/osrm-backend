738c3eae912c12c7f2810c5d9c892ce934a162cf
DennisOSRM <luxen@kit.edu>
2011-12-23 17:28:12 +0100

    void function needs to be void

--- Util/OpenMPReplacement.h
+++ Util/OpenMPReplacement.h
@@ -28,7 +28,7 @@ or see http://www.gnu.org/licenses/agpl.txt.
 inline const int omp_get_num_procs() { return 1; }
 inline const int omp_get_max_threads() { return 1; }
 inline const int omp_get_thread_num() { return 0; }
-inline const int omp_set_num_threads(int i) {}
+inline const void omp_set_num_threads(int i) {}
 #endif
 
 #endif
