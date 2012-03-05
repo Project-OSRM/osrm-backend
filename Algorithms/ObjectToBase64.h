c273351d4a8201306d17c8c3a3f884ce42c0e5bc
DennisOSRM <luxen@kit.edu>
2012-03-05 15:44:21 +0100

    PhantomNodes get transmitted as well. The client is supposed to send us this information whenever it's possible.

---
+++ Algorithms/ObjectToBase64.h
@@ -0,0 +1,58 @@
+/*
+    open source routing machine
+    Copyright (C) Dennis Luxen, others 2010
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU AFFERO General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+or see http://www.gnu.org/licenses/agpl.txt.
+ */
+
+#ifndef OBJECTTOBASE64_H_
+#define OBJECTTOBASE64_H_
+
+#include <boost/archive/iterators/base64_from_binary.hpp>
+#include <boost/archive/iterators/binary_from_base64.hpp>
+#include <boost/archive/iterators/transform_width.hpp>
+#include <boost/foreach.hpp>
+
+#include <algorithm>
+#include <string>
+
+#include "../Util/StringUtil.h"
+
+typedef
+    boost::archive::iterators::base64_from_binary<
+        boost::archive::iterators::transform_width<string::const_iterator, 6, 8>
+> base64_t;
+
+typedef
+    boost::archive::iterators::transform_width<
+        boost::archive::iterators::binary_from_base64<string::const_iterator>, 8, 6
+> binary_t;
+
+template<class ToEncodeT>
+static void EncodeObjectToBase64(const ToEncodeT & object, std::string& encoded) {
+    assert(0 == encoded.length());
+    char * pointerToOriginalObject = (char *)&object;
+    encoded = std::string(base64_t(pointerToOriginalObject), base64_t(pointerToOriginalObject+sizeof(ToEncodeT)));
+}
+
+template<class ToEncodeT>
+static void DecodeObjectFraBase64(ToEncodeT & object, const std::string& encoded) {
+    char * pointerToDecodedObject = (char *)&object;
+    std::string dec(binary_t(encoded.begin()), binary_t(encoded.begin() + encoded.length() - 1));
+    std::copy ( dec.begin(), dec.end(), pointerToDecodedObject );
+}
+
+#endif /* OBJECTTOBASE64_H_ */
