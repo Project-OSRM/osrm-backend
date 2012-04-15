f08c389faa401e3e6dc6e20fa1ef1090974b3943
DennisSchiefer <shiin@gmx.de>
2012-04-15 19:35:26 +0200

    moved version to 0.1.4

--- WebContent/OSRM.base.js
+++ WebContent/OSRM.base.js
@@ -19,8 +19,8 @@ or see http://www.gnu.org/licenses/agpl.txt.
 // [has to be loaded before all other OSRM classes]
 
 var OSRM = {};
-OSRM.VERSION = '0.1.3';
-OSRM.DATE = '120402';
+OSRM.VERSION = '0.1.4';
+OSRM.DATE = '120416';
 OSRM.CONSTANTS = {};
 OSRM.DEFAULTS = {};
 OSRM.GLOBALS = {};
