f6173b7a4886ed360ce28ae94ca4ec8caeeb494e
JeanFred <JeanFred1@gmail.com>
2012-04-23 22:14:27 +0200

    Add localisation in French

---
+++ WebContent/localization/OSRM.Locale.fr.js
@@ -0,0 +1,90 @@
+/*
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU AFFERO General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+or see http://www.gnu.org/licenses/agpl.txt.
+*/
+
+// OSRM localization
+// [French language support]
+
+
+OSRM.Localization["fr"] = {
+//gui
+"OPEN_JOSM": "JOSM",
+"OPEN_OSMBUGS": "Bugs OSM",
+"GUI_START": "Départ",
+"GUI_END": "Arrivée",
+"GUI_RESET": "&nbsp;&nbsp;Réinitialiser&nbsp;&nbsp;",
+"GUI_SEARCH": "&nbsp;&nbsp;Montrer&nbsp;&nbsp;",
+"GUI_REVERSE": "Inverser",
+"GUI_OPTIONS": "Outils de cartographie",
+"GUI_HIGHLIGHT_UNNAMED_ROADS": "Surligner les rues sans nom",
+"GUI_START_TOOLTIP": "Entrez le lieu de départ",
+"GUI_END_TOOLTIP": "Entrez le lieu d’arrivée",
+"GUI_LEGAL_NOTICE": "GUI2 v"+OSRM.VERSION+" "+OSRM.DATE+" - hébergement de OSRM par <a href='http://algo2.iti.kit.edu/'>KIT</a> - géocodage par <a href='http://www.osm.org/'>OSM</a>",
+// geocoder
+"SEARCH_RESULTS": "Résultats de recherche",
+"FOUND_X_RESULTS": "%i résultat(s)",
+"TIMED_OUT": "La recherche n’a pas abouti",
+"NO_RESULTS_FOUND": "Aucun résultat trouvé",
+"NO_RESULTS_FOUND_SOURCE": "Aucun résultat pour le départ",
+"NO_RESULTS_FOUND_TARGET": "Aucun résultat pour l'arrivée",
+//routing
+"ROUTE_DESCRIPTION": "Description de l’itinéraire",
+"GET_LINK_TO_ROUTE": "Générer un lien",
+"GENERATE_LINK_TO_ROUTE": "en attente du lien",
+"LINK_TO_ROUTE_TIMEOUT": "indisponible",
+"GPX_FILE": "Fichier GPX",
+"DISTANCE": "Distance",
+"DURATION": "Durée",
+"YOUR_ROUTE_IS_BEING_COMPUTED": "Votre itinéraire est en cours de calcul",
+"NO_ROUTE_FOUND": "Pas d’itinéraire possible",
+// directions
+"N": "nord",
+"E": "est",
+"S": "sud",
+"W": "ouest",
+"NE": "nord-est",
+"SE": "sud-est",
+"SW": "sud-ouest",
+"NW": "nord-ouest",
+// driving directions
+// %s: road name
+// [*]: will only be printed when there actually is a road name
+"DIRECTION_0":"Instruction inconnue[ sur <b>%s</b>]",
+"DIRECTION_1":"Continuez[ sur <b>%s</b>]",
+"DIRECTION_2":"Tournez légèrement à droite[ sur <b>%s</b>]",
+"DIRECTION_3":"Tournez à droite[ sur <b>%s</b>]",
+"DIRECTION_4":"Tournez fortement à droite[ sur <b>%s</b>]",
+"DIRECTION_5":"Faites demi-tour[ sur <b>%s</b>]",
+"DIRECTION_6":"Tournez légèrement à gauche[ sur <b>%s</b>]",
+"DIRECTION_7":"Tournez à gauche[ sur <b>%s</b>]",
+"DIRECTION_8":"Tournez fortement à gauche[ sur <b>%s</b>]",
+"DIRECTION_10":"Direction <b>%s</b>",
+"DIRECTION_11-1":"Au rond-point, prenez la première sortie[ sur <b>%s</b>]",
+"DIRECTION_11-2":"Au rond-point, prenez la deuxième sortie[ sur <b>%s</b>]",
+"DIRECTION_11-3":"Au rond-point, prenez la troisième sortie[ sur <b>%s</b>]",
+"DIRECTION_11-4":"Au rond-point, prenez la quatrième sortie[ sur <b>%s</b>]",
+"DIRECTION_11-5":"Au rond-point, prenez la cinquième sortie[ sur <b>%s</b>]",
+"DIRECTION_11-6":"Au rond-point, prenez la sixième sortie[ sur <b>%s</b>]",
+"DIRECTION_11-7":"Au rond-point, prenez la septième sortie[ sur <b>%s</b>]",
+"DIRECTION_11-8":"Au rond-point, prenez la huitième sortie[ sur <b>%s</b>]",
+"DIRECTION_11-9":"Au rond-point, prenez la neuvième sortie[ sur <b>%s</b>]",
+"DIRECTION_11-x":"Au rond-point, prenez l’une des trop nombreuses sorties[ sur <b>%s</b>]",
+"DIRECTION_15":"Vous êtes arrivé"
+};
+
+//set GUI language on load
+if( OSRM.DEFAULTS.LANUGAGE_ONDEMAND_RELOADING == true )
+	OSRM.Localization.setLanguage("fr");
