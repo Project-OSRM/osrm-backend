04298b73f3b9f98d084d7bcc36c0fe05adfe9a5e
shiin <shiin@T42>
2012-04-24 23:06:19 +0200

    added image for "head on", removed unneeded images

--- WebContent/main.js
+++ WebContent/main.js
@@ -71,6 +71,7 @@ OSRM.prefetchImages = function() {
 		              	{id:'direction_6',						url:'images/slight-left.png'},
 		              	{id:'direction_7',						url:'images/turn-left.png'},
 		              	{id:'direction_8',						url:'images/sharp-left.png'},
+		              	{id:'direction_10',						url:'images/head.png'},
 		        		{id:'direction_11',						url:'images/round-about.png'},
 		        		{id:'direction_15',						url:'images/target.png'}
 	               ];
