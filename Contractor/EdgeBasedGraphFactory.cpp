fe35fefc75f6d9042cb6d7ea2a39bc7822a7683b
DennisOSRM <luxen@kit.edu>
2012-05-15 11:40:41 +0200

    Reserving additional 20% in edge-based graph factory per resize.

--- Contractor/EdgeBasedGraphFactory.cpp
+++ Contractor/EdgeBasedGraphFactory.cpp
@@ -256,10 +256,10 @@ void EdgeBasedGraphFactory::Run(const char * originalEdgeDataFilename) {
                         //distance += ComputeTurnPenalty(u, v, w);
                         assert(edgeData1.edgeBasedNodeID != edgeData2.edgeBasedNodeID);
                         if(edgeBasedEdges.size() == edgeBasedEdges.capacity()-3) {
-                            edgeBasedEdges.reserve(edgeBasedEdges.size()*1.1);
+                            edgeBasedEdges.reserve(edgeBasedEdges.size()*1.2);
                         }
                         if(originalEdgeData.size() == originalEdgeData.capacity()-3) {
-                            originalEdgeData.reserve(originalEdgeData.size()*1.1);
+                            originalEdgeData.reserve(originalEdgeData.size()*1.2);
                         }
                         OriginalEdgeData oed(v,edgeData2.nameID, turnInstruction);
                         EdgeBasedEdge newEdge(edgeData1.edgeBasedNodeID, edgeData2.edgeBasedNodeID, edgeBasedEdges.size(), distance, true, false );
