59775a9864aaa5b4d49fda1a9b49064adbd8a24c
shiin <shiin@T42>
2012-06-17 18:23:57 +0200

    more towards alternatives

--- WebContent/routing/OSRM.RoutingAlternatives.js
+++ WebContent/routing/OSRM.RoutingAlternatives.js
@@ -21,72 +21,87 @@ or see http://www.gnu.org/licenses/agpl.txt.
 
 OSRM.RoutingAlternatives = {
 		
+// remaining problems: how to handle PRINTING (do it internally), LINKS (add parameter to JSONP call)
+		
+// name of gui buttons to choose alternatives
+_buttons: ["gui_a", "gui_b"],
+		
 // prepare response
-prepare: function() {
-	OSRM.RoutingAlternatives._buildAlternatives();
+prepare: function(response) {
+	var original = {};
+	original.route_geometry = response.route_geometry;
+	original.route_instructions = response.route_instructions;
+	original.route_summary = response.route_summary;
+	OSRM.G.response.alternatives.push( original );
+	
+	OSRM.G.response.active_alternative = 0;
+	
+	OSRM.RoutingAlternatives._addGUI();
+},
+prepare_Redraw: function(response) {
+	var original = {};
+	original.route_geometry = response.route_geometry;
+	original.route_instructions = response.route_instructions;
+	original.route_summary = response.route_summary;
+	OSRM.G.response.alternatives = response.alternatives;
+	OSRM.G.response.alternatives.push( original );
+	
+	OSRM.RoutingAlternatives._addGUI();
+},
+
+// press one of the buttons
+press: function(button_id) {
+	var buttons = OSRM.RoutingAlternatives._buttons;
+	for(var i=0, size=buttons; i<size; i++) {
+		if(button_id == i)
+			document.getElementById( buttons[i] ).className = "button-pressed top-right-button";
+		else
+			document.getElementById( buttons[i] ).className = "button top-right-button";
+	}
+	OSRM.G.response.active_alternative = button_id;	
+},
+
+// mouse events on buttons
+_click: function(button_id) {
+	if( OSRM.G.response.active_alternative == button_id )
+		return;
+	OSRM.RoutingAlternatives.press(button_id);
+	OSRM.G.route.hideAlternativeRoute();
+	// switch data
+	//OSRM.Routing.showRoute(OSRM.G.response);
+	console.log("click "+button_id);	
+},
+_mouseover: function(button_id) {
+	if( OSRM.G.response.active_alternative == button_id )
+		return;
+	
+	var geometry = OSRM.RoutingGeometry._decode(OSRM.G.response.route_geometry, 5);
+	OSRM.G.route.showAlternativeRoute(geometry);
+	console.log("over "+button_id);	
+},
+_mouseout: function(button_id) {
+	if( OSRM.G.response.active_alternative == button_id )
+		return;
+	
+	OSRM.G.route.hideAlternativeRoute();
+	console.log("out "+button_id);	
 },
 
 // add alternatives
-_buildAlternatives: function() {
+_addGUI: function() {
 	var data =
 		'<a class="button top-right-button" id="gui-b">B</a>' +
-		'<a class="button-pressed top-right-button" id="gui-a">A</a>';
+		'<a class="button top-right-button" id="gui-a">A</a>';
 	document.getElementById('information-box-header').innerHTML = data + document.getElementById('information-box-header').innerHTML;
-	OSRM.G.response.active = 0;
 	
-	document.getElementById("gui-a").onclick = function () {
-		if( OSRM.G.response.active == 0 )
-			return;
-		document.getElementById("gui-a").className = "button-pressed top-right-button";
-		document.getElementById("gui-b").className = "button top-right-button";
-		OSRM.G.route.hideAlternativeRoute();
-		OSRM.G.response.active = 0;
-		// switch data
-		//OSRM.Routing.showRoute(OSRM.G.response);
-		console.log("click a");		
-	};
-	document.getElementById("gui-a").onmouseover = function () {
-		if( OSRM.G.response.active == 0 )
-			return;
+	OSRM.RoutingAlternatives.press( OSRM.G.response.active_alternative );
 		
-		var geometry = OSRM.RoutingGeometry._decode(OSRM.G.response.route_geometry, 5);
-		OSRM.G.route.showAlternativeRoute(geometry);
-		console.log("over a");
-	};
-	document.getElementById("gui-a").onmouseout = function () {
-		if( OSRM.G.response.active == 0 )
-			return;
-		
-		OSRM.G.route.hideAlternativeRoute();
-		console.log("out a");
-	};
-	
-	document.getElementById("gui-b").onclick = function ()  {
-		if( OSRM.G.response.active == 1 )
-			return;
-		document.getElementById("gui-a").className = "button top-right-button";
-		document.getElementById("gui-b").className = "button-pressed top-right-button";
-		OSRM.G.route.hideAlternativeRoute();
-		OSRM.G.response.active = 1;
-		// switch data
-		//OSRM.Routing.showRoute(OSRM.G.response);
-		console.log("click b");
-	};
-	document.getElementById("gui-b").onmouseover = function () {
-		if( OSRM.G.response.active == 1 )
-			return;
-		
-		var geometry = OSRM.RoutingGeometry._decode(OSRM.G.response.route_geometry, 5);
-		OSRM.G.route.showAlternativeRoute(geometry);		
-		console.log("over b"); 
-	};	
-	document.getElementById("gui-b").onmouseout = function () {
-		if( OSRM.G.response.active == 1 )
-			return;
-		
-		OSRM.G.route.hideAlternativeRoute();
-		console.log("out b");
-	};	
+	var buttons = OSRM.RoutingAlternatives._buttons;
+	for(var i=0, size=buttons; i<size; i++) {
+		document.getElementById(buttons[i]).onclick = function () { OSRM.RoutingAlternatives._click(i); };
+		document.getElementById(buttons[i]).onmouseover = function () { OSRM.RoutingAlternatives._mouseover(i); };
+		document.getElementById(buttons[i]).onmouseout = function () { OSRM.RoutingAlternatives._mouseout(i); };
+	}
 }
 
 };
