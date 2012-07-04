22783a53cd3560bf7639c943bd14cc545897b419
DennisSchiefer <shiin@gmx.de>
2012-07-04 17:25:04 +0100

    wrapper function for localization pulldown menu to work as trigger function for notifications

--- WebContent/localization/OSRM.Localization.js
+++ WebContent/localization/OSRM.Localization.js
@@ -42,11 +42,14 @@ init: function() {
 	}
 	
 	// generate selectors
-	OSRM.GUI.selectorInit("gui-language-toggle", options, selected, OSRM.Localization.setLanguage);
-	OSRM.GUI.selectorInit("gui-language-2-toggle", options_2, selected, OSRM.Localization.setLanguage);
+	OSRM.GUI.selectorInit("gui-language-toggle", options, selected, OSRM.Localization.setLanguageWrapper);
+	OSRM.GUI.selectorInit("gui-language-2-toggle", options_2, selected, OSRM.Localization.setLanguageWrapper);
 	
 	// set default language
 	OSRM.Localization.setLanguage( OSRM.DEFAULTS.LANGUAGE );
+},
+setLanguageWrapper: function(language) {
+	OSRM.Localization.setLanguage(language);
 },
 setLanguage: function(language) {
 	// change value of both language selectors
