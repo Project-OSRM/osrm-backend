7222231abf99255611f69f89dc6280aa60e0b9be
DennisOSRM <luxen@kit.edu>
2012-08-30 18:58:19 +0200

    Removing unnecessary defines

--- extractor.cpp
+++ extractor.cpp
@@ -18,11 +18,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 or see http://www.gnu.org/licenses/agpl.txt.
  */
 
-#ifdef STXXL_VERBOSE_LEVEL
-#undef STXXL_VERBOSE_LEVEL
-#endif
-#define STXXL_VERBOSE_LEVEL -1000
-
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
