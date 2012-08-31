cdaef51b2033c3cfaf9eba28b740096704285359
DennisSchiefer <shiin@gmx.de>
2012-08-31 07:41:01 +0100

    version push to 0.1.8

--- WebContent/OSRM.base.js
+++ WebContent/OSRM.base.js
@@ -19,8 +19,8 @@ or see http://www.gnu.org/licenses/agpl.txt.
 // [has to be loaded before all other OSRM classes]
 
 var OSRM = {};
-OSRM.VERSION = '0.1.7';
-OSRM.DATE = '120705';
+OSRM.VERSION = '0.1.8';
+OSRM.DATE = '120831';
 OSRM.CONSTANTS = {};
 OSRM.DEFAULTS = {};
 OSRM.GLOBALS = {};
