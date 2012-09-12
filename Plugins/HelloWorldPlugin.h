07b6073dcfec47d09bcacf0d7689dfed74df2d9f
DennisOSRM <luxen@kit.edu>
2012-09-12 15:15:21 +0200

    Make hello world plugin more verbose to allow testing

--- Plugins/HelloWorldPlugin.h
+++ Plugins/HelloWorldPlugin.h
@@ -21,18 +21,27 @@ public:
 	std::string GetVersionString() const { return std::string("0.1a"); }
 
 	void HandleRequest(const RouteParameters & routeParameters, http::Reply& reply) {
-		std::cout << "[hello world]: runnning handler" << std::endl;
 		reply.status = http::Reply::ok;
 		reply.content.append("<html><head><title>Hello World Demonstration Document</title></head><body><h1>Hello, World!</h1>");
 		std::stringstream content;
-        content << "Number of locations: " << routeParameters.coordinates.size() << "<br>\n";
+		content << "<pre>";
+        content << "zoom level: " << routeParameters.zoomLevel << "\n";
+        content << "checksum: " << routeParameters.checkSum << "\n";
+        content << "instructions: " << (routeParameters.printInstructions ? "yes" : "no") << "\n";
+        content << "geometry: " << (routeParameters.geometry ? "yes" : "no") << "\n";
+        content << "compression: " << (routeParameters.compression ? "yes" : "no") << "\n";
+        content << "output format: " << routeParameters.outputFormat << "\n";
+        content << "json parameter: " << routeParameters.jsonpParameter << "\n";
+        content << "language: " << routeParameters.language << "<br>";
+        content << "Number of locations: " << routeParameters.coordinates.size() << "\n";
         for(unsigned i = 0; i < routeParameters.coordinates.size(); ++i) {
-            content << "  [" << i << "] " << routeParameters.coordinates[i].lat/100000. << "," << routeParameters.coordinates[i].lon/100000. << "<br>\n";
+            content << "  [" << i << "] " << routeParameters.coordinates[i].lat/100000. << "," << routeParameters.coordinates[i].lon/100000. << "\n";
         }
-        content << "Number of hints: " << routeParameters.hints.size() << "<br>\n";
+        content << "Number of hints: " << routeParameters.hints.size() << "\n";
         for(unsigned i = 0; i < routeParameters.hints.size(); ++i) {
-            content << "  [" << i << "] " << routeParameters.hints[i] << "<br>\n";
+            content << "  [" << i << "] " << routeParameters.hints[i] << "\n";
         }
+        content << "</pre>";
 		reply.content.append(content.str());
 		reply.content.append("</body></html>");
 	}
