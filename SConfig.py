df9a8a32eb0a7c99cb9f4afcf8c4ef9816040895
DennisOSRM <luxen@kit.edu>
2012-09-25 17:40:22 +0200

    Adding new component analysis tool
    Conflicts:
    
    	SConstruct

---
+++ SConfig.py
@@ -0,0 +1,22 @@
+# THIS FILE WAS AUTOMATICALLY GENERATED. MANUAL CHANGES IN THIS FILE WILL BE LOST AFTER THE NEXT C/C++ PROJECT CHANGES!!!
+SCONS_OPTIONS = {'num_jobs':'4'}
+DECIDER = 'MD5'
+COMP_INCLUDES_INTO_CCFLAGS = False
+BUILD_ARTIFACT_NAME = 'Project-OSRM'
+PROJECT_TYPE = 'exe'
+BUILD_CONFIGURATION = 'Debug'
+TOOLCHAIN_NAME = 'linux gcc'
+C_FLAGS = '-O0 -g3 -Wall -c -fmessage-length=0'
+CXX_FLAGS = '-DBOOST_FOREACH(a,b)=for(a:b) -I/usr/include/libxml2 -O0 -g3 -Wall -c -fmessage-length=0'
+COMPILER_NAME = 'g++'
+PROJECT_NAME = 'Project-OSRM'
+INCLUDES = ['/usr/include/libxml2']
+LIBRARIES = []
+LIBRARY_PATHS = []
+SOURCE_PATHS = {'Project-OSRM':['ThirdParty']}
+PRE_BUILD_COMMAND = ''
+PRE_BUILD_DESC = ''
+POST_BUILD_COMMAND = ''
+POST_BUILD_DESC = ''
+COMPILER_DEFINES = {'BOOST_FOREACH(a,b)':'for(a:b)'}
+LINKER_FLAGS = ''
