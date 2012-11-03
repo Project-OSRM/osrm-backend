24321d860cffdb5fc996682c6aaf0a99231d7a5c
DennisOSRM <luxen@kit.edu>
2012-11-03 08:18:22 +0100

    Moving more functions into LUA

--- Extractor/ExtractionHelperFunctions.h
+++ Extractor/ExtractionHelperFunctions.h
@@ -25,6 +25,8 @@ or see http://www.gnu.org/licenses/agpl.txt.
 
 #include <climits>
 
+//TODO: Move into LUA
+
 inline bool durationIsValid(const std::string &s) {
     boost::regex e ("((\\d|\\d\\d):)*(\\d|\\d\\d)",boost::regex_constants::icase|boost::regex_constants::perl);
 
