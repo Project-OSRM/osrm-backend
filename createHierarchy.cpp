d3dce3cc257dd520d864cb0f19568d4bb8ed2ccd
Dennis Luxen <luxen@kit.edu>
2012-12-11 23:45:14 +0100

    Remove unused defines

--- createHierarchy.cpp
+++ createHierarchy.cpp
@@ -18,14 +18,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 or see http://www.gnu.org/licenses/agpl.txt.
  */
 
-#define VERBOSE(x) x
-#define VERBOSE2(x)
-
-#ifdef NDEBUG
-#undef VERBOSE
-#undef VERBOSE2
-#endif
-
 extern "C" {
 #include <lua.h>
 #include <lauxlib.h>
