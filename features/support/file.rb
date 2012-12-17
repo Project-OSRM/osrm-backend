5a5b4eb1f74e30d1a337c22a1a8cc6c7934599c4
Emil Tin <emil.tin@tmf.kk.dk>
2012-12-17 17:18:41 +0100

    output last lines from log if binary exists during testing

---
+++ features/support/file.rb
@@ -0,0 +1,22 @@
+class File
+  
+  #read last n lines of a file. useful for getting last part of a big log file.
+  def tail(n)
+    buffer = 1024
+    idx = (size - buffer).abs
+    chunks = []
+    lines = 0
+
+    begin
+      seek(idx)
+      chunk = read(buffer)
+      lines += chunk.count("\n")
+      chunks.unshift chunk
+      idx -= buffer
+    end while lines < ( n + 1 ) && pos != 0
+
+    tail_of_file = chunks.join('')
+    ary = tail_of_file.split(/\n/)
+    lines_to_return = ary[ ary.size - n, ary.size - 1 ]
+  end
+end
