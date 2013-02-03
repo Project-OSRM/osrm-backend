b3d517943a122e132052436f6c6892c7b99d838f
Emil Tin <emil.tin@tmf.kk.dk>
2013-02-03 13:07:30 +0100

    fuzzy ranges on routability tables, value shortcuts

---
+++ features/support/fuzzy.rb
@@ -0,0 +1,26 @@
+
+class FuzzyMatch
+
+  def self.match got, want
+    if got == want
+      return true
+    elsif want.match /(.*)\s+~(.+)%$/       #percentage range: 100 ~5%
+      margin = 1 - $2.to_f*0.01
+      from = $1.to_f*margin
+      to = $1.to_f/margin
+      return got.to_f >= from && got.to_f <= to
+    elsif want.match /(.*)\s+\+\-(.+)$/    #absolute range: 100 +-5
+      margin = $2.to_f
+      from = $1.to_f-margin
+      to = $1.to_f+margin
+      return got.to_f >= from && got.to_f <= to
+    elsif want =~ /^\/(.*)\/$/             #regex: /a,b,.*/
+      return got =~ /#{$1}/
+    else
+      return false
+    end      
+  end
+
+end
+
+
