9d6bd9127923171309c97112d0d6d09ef57af1a3
Dennis Luxen <luxen@kit.edu>
2013-06-26 13:39:45 -0400

    some optimization to speed up pbf parsing

--- Extractor/PBFParser.h
+++ Extractor/PBFParser.h
@@ -1,17 +1,17 @@
 /*
  open source routing machine
  Copyright (C) Dennis Luxen, others 2010
- 
+
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU AFFERO General Public License as published by
  the Free Software Foundation; either version 3 of the License, or
  any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU Affero General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -21,6 +21,13 @@
 #ifndef PBFPARSER_H_
 #define PBFPARSER_H_
 
+#include "../DataStructures/HashTable.h"
+#include "../DataStructures/ConcurrentQueue.h"
+#include "../Util/MachineInfo.h"
+#include "../Util/OpenMPWrapper.h"
+#include "../typedefs.h"
+
+#include "BaseParser.h"
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/ref.hpp>
@@ -30,44 +37,38 @@
 
 #include <zlib.h>
 
-#include "../typedefs.h"
-#include "../DataStructures/HashTable.h"
-#include "../DataStructures/ConcurrentQueue.h"
-#include "../Util/MachineInfo.h"
-#include "../Util/OpenMPWrapper.h"
 
-#include "BaseParser.h"
 
 class PBFParser : public BaseParser {
-    
+
     enum EntityType {
         TypeNode = 1,
         TypeWay = 2,
         TypeRelation = 4,
         TypeDenseNode = 8
     } ;
-    
+
     struct _ThreadData {
         int currentGroupID;
         int currentEntityID;
         short entityTypeIndicator;
-        
+
         OSMPBF::BlobHeader PBFBlobHeader;
         OSMPBF::Blob PBFBlob;
-        
+
         OSMPBF::HeaderBlock PBFHeaderBlock;
         OSMPBF::PrimitiveBlock PBFprimitiveBlock;
-        
+
         std::vector<char> charBuffer;
     };
-    
+
 public:
     PBFParser(const char * fileName, ExtractorCallbacks* ec, ScriptingEnvironment& se);
     virtual ~PBFParser();
-    
+
     inline bool ReadHeader();
 	inline bool Parse();
-    
+
 private:
     inline void ReadData();
     inline void ParseData();
@@ -75,7 +76,7 @@ private:
     inline void parseNode(_ThreadData * );
     inline void parseRelation(_ThreadData * threadData);
     inline void parseWay(_ThreadData * threadData);
-    
+
     inline void loadGroup(_ThreadData * threadData);
     inline void loadBlock(_ThreadData * threadData);
     inline bool readPBFBlobHeader(std::fstream& stream, _ThreadData * threadData);
@@ -83,17 +84,17 @@ private:
     inline bool unpackLZMA(std::fstream &, _ThreadData * );
     inline bool readBlob(std::fstream& stream, _ThreadData * threadData) ;
     inline bool readNextBlock(std::fstream& stream, _ThreadData * threadData);
-    
+
     static const int NANO = 1000 * 1000 * 1000;
     static const int MAX_BLOB_HEADER_SIZE = 64 * 1024;
     static const int MAX_BLOB_SIZE = 32 * 1024 * 1024;
-    
+
 #ifndef NDEBUG
     /* counting the number of read blocks and groups */
     unsigned groupCount;
     unsigned blockCount;
 #endif
-	
+
     std::fstream input;     // the input stream to parse
     boost::shared_ptr<ConcurrentQueue < _ThreadData* > > threadDataQueue;
 };
