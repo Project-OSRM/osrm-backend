8d98316a892936167415677e61d21b1e8b2f6a11
Dennis Luxen <luxen@kit.edu>
2013-10-15 11:52:41 +0200

    change EdgeBasedNode namespace

--- createHierarchy.cpp
+++ createHierarchy.cpp
@@ -202,7 +202,7 @@ int main (int argc, char *argv[]) {
         NodeID edgeBasedNodeNumber = edgeBasedGraphFactory->GetNumberOfNodes();
         DeallocatingVector<EdgeBasedEdge> edgeBasedEdgeList;
         edgeBasedGraphFactory->GetEdgeBasedEdges(edgeBasedEdgeList);
-        std::vector<EdgeBasedGraphFactory::EdgeBasedNode> nodeBasedEdgeList;
+        std::vector<EdgeBasedNode> nodeBasedEdgeList;
         edgeBasedGraphFactory->GetEdgeBasedNodes(nodeBasedEdgeList);
         delete edgeBasedGraphFactory;
 
@@ -228,14 +228,14 @@ int main (int argc, char *argv[]) {
          */
 
         SimpleLogger().Write() << "building r-tree ...";
-        StaticRTree<EdgeBasedGraphFactory::EdgeBasedNode> * rtree =
-                new StaticRTree<EdgeBasedGraphFactory::EdgeBasedNode>(
+        StaticRTree<EdgeBasedNode> * rtree =
+                new StaticRTree<EdgeBasedNode>(
                         nodeBasedEdgeList,
                         rtree_nodes_path.c_str(),
                         rtree_leafs_path.c_str()
                 );
         delete rtree;
-        IteratorbasedCRC32<std::vector<EdgeBasedGraphFactory::EdgeBasedNode> > crc32;
+        IteratorbasedCRC32<std::vector<EdgeBasedNode> > crc32;
         unsigned crc32OfNodeBasedEdgeList = crc32(nodeBasedEdgeList.begin(), nodeBasedEdgeList.end() );
         nodeBasedEdgeList.clear();
         SimpleLogger().Write() << "CRC32: " << crc32OfNodeBasedEdgeList;
