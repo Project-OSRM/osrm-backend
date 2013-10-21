b7db65a023448dd17f2005fcf9f9b4de768b9b9b
Dennis Luxen <luxen@kit.edu>
2013-10-21 19:07:49 +0200

    rename condidition variable and add mutex for pending update

--- Server/DataStructures/SharedBarriers.h
+++ Server/DataStructures/SharedBarriers.h
@@ -6,11 +6,12 @@ struct SharedBarriers {
    SharedBarriers () :  update_ongoing(false), number_of_queries(0) { }
 
    // Mutex to protect access to the boolean variable
-   boost::interprocess::interprocess_mutex      update_mutex;
-   boost::interprocess::interprocess_mutex      query_mutex;
+   boost::interprocess::interprocess_mutex   pending_update_mutex;
+   boost::interprocess::interprocess_mutex   update_mutex;
+   boost::interprocess::interprocess_mutex   query_mutex;
 
    // Condition that no update is running
-   boost::interprocess::interprocess_condition  update_finished_condition;
+   boost::interprocess::interprocess_condition  no_running_queries_condition;
 
    // Is there an ongoing update?
    bool update_ongoing;
