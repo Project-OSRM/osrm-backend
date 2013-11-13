125877035f65a9b3e39fc8a8b319882344b5bb86
Dennis Luxen <info@project-osrm.org>
2013-11-13 17:48:58 -0500

    move polyline generalizer into compile unit

--- Algorithms/DouglasPeucker.h
+++ Algorithms/DouglasPeucker.h
@@ -46,10 +46,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * indicating if the points is present in the generalization.
  * Note: points may also be pre-selected*/
 
-//These thresholds are more or less heuristically chosen.
-//                                                  0          1         2         3         4          5         6         7       8       9       10     11       12     13    14   15  16  17   18
-static double DouglasPeuckerThresholds[19] = { 32000000., 16240000., 80240000., 40240000., 20000000., 10000000., 500000., 240000., 120000., 60000., 30000., 19000., 5000., 2000., 200, 16,  6, 3. , 3. };
-
 class DouglasPeucker {
 private:
     typedef std::pair<std::size_t, std::size_t> PairOfPoints;
@@ -60,110 +56,14 @@ private:
      * This distance computation does integer arithmetic only and is about twice as fast as
      * the other distance function. It is an approximation only, but works more or less ok.
      */
-    inline int fastDistance(const FixedPointCoordinate& point, const FixedPointCoordinate& segA, const FixedPointCoordinate& segB) const {
-        const int p2x = (segB.lon - segA.lat);
-        const int p2y = (segB.lon - segA.lat);
-        const int something = p2x*p2x + p2y*p2y;
-        int u = ( 0 == something  ? 0 : ((point.lon - segA.lon) * p2x + (point.lat - segA.lat) * p2y) / something);
-
-        if (u > 1) {
-            u = 1;
-        } else if (u < 0) {
-            u = 0;
-        }
-
-        const int x = segA.lon + u * p2x;
-        const int y = segA.lat + u * p2y;
-
-        const int dx = x - point.lon;
-        const int dy = y - point.lat;
-
-        const int dist = (dx*dx + dy*dy);
-
-        return dist;
-    }
-
-
+    int fastDistance(
+        const FixedPointCoordinate& point,
+        const FixedPointCoordinate& segA,
+        const FixedPointCoordinate& segB
+    ) const;
 public:
-    void Run(std::vector<SegmentInformation> & input_geometry, const unsigned zoom_level) {
-        {
-            BOOST_ASSERT_MSG(zoom_level < 19, "unsupported zoom level");
-            BOOST_ASSERT_MSG(1 < input_geometry.size(), "geometry invalid");
-            std::size_t left_border = 0;
-            std::size_t right_border = 1;
-            //Sweep linerarily over array and identify those ranges that need to be checked
-            do {
-                BOOST_ASSERT_MSG(
-                    input_geometry[left_border].necessary,
-                    "left border must be necessary"
-                );
-                BOOST_ASSERT_MSG(
-                    input_geometry.back().necessary,
-                    "right border must be necessary"
-                );
+    void Run(std::vector<SegmentInformation> & input_geometry, const unsigned zoom_level);
 
-                if(input_geometry[right_border].necessary) {
-                    recursion_stack.push(std::make_pair(left_border, right_border));
-                    left_border = right_border;
-                }
-                ++right_border;
-            } while( right_border < input_geometry.size());
-        }
-        while(!recursion_stack.empty()) {
-            //pop next element
-            const PairOfPoints pair = recursion_stack.top();
-            recursion_stack.pop();
-            BOOST_ASSERT_MSG(
-                input_geometry[pair.first].necessary,
-                "left border mus be necessary"
-            );
-            BOOST_ASSERT_MSG(
-                input_geometry[pair.second].necessary,
-                "right border must be necessary"
-            );
-            BOOST_ASSERT_MSG(
-                pair.second < input_geometry.size(),
-                "right border outside of geometry"
-            );
-            BOOST_ASSERT_MSG(
-                pair.first < pair.second,
-                "left border on the wrong side"
-            );
-            int max_distance = INT_MIN;
-
-            std::size_t farthest_element_index = pair.second;
-            //find index idx of element with max_distance
-            for(std::size_t i = pair.first+1; i < pair.second; ++i){
-                const int temp_dist = fastDistance(
-                                        input_geometry[i].location,
-                                        input_geometry[pair.first].location,
-                                        input_geometry[pair.second].location
-                                    );
-                const double distance = std::fabs(temp_dist);
-                if(
-                    distance > DouglasPeuckerThresholds[zoom_level] &&
-                    distance > max_distance
-                ) {
-                    farthest_element_index = i;
-                    max_distance = distance;
-                }
-            }
-            if (max_distance > DouglasPeuckerThresholds[zoom_level]) {
-                //  mark idx as necessary
-                input_geometry[farthest_element_index].necessary = true;
-                if (1 < (farthest_element_index - pair.first) ) {
-                    recursion_stack.push(
-                        std::make_pair(pair.first, farthest_element_index)
-                    );
-                }
-                if (1 < (pair.second - farthest_element_index) ) {
-                    recursion_stack.push(
-                        std::make_pair(farthest_element_index, pair.second)
-                    );
-                }
-            }
-        }
-    }
 };
 
 #endif /* DOUGLASPEUCKER_H_ */
