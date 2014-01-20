9db36584c2ac2ca6b8e332f3f5ca9f2ef5e7e664
alex85k <alexei_kasatkin@mail.ru>
2014-01-20 23:21:42 +0600

    fix libraries and symbol redefinitions issues for MSVC

--- RoutingAlgorithms/BasicRoutingInterface.h
+++ RoutingAlgorithms/BasicRoutingInterface.h
@@ -40,13 +40,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <stack>
 
-SearchEngineData::SearchEngineHeapPtr SearchEngineData::forwardHeap;
+/*SearchEngineData::SearchEngineHeapPtr SearchEngineData::forwardHeap;
 SearchEngineData::SearchEngineHeapPtr SearchEngineData::backwardHeap;
 SearchEngineData::SearchEngineHeapPtr SearchEngineData::forwardHeap2;
 SearchEngineData::SearchEngineHeapPtr SearchEngineData::backwardHeap2;
 SearchEngineData::SearchEngineHeapPtr SearchEngineData::forwardHeap3;
 SearchEngineData::SearchEngineHeapPtr SearchEngineData::backwardHeap3;
-
+*/
 template<class DataFacadeT>
 class BasicRoutingInterface : boost::noncopyable {
 private:
