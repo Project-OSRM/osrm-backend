da81e4839a62c6aa3c2b2eb0a27470259a15bb21
Dennis Luxen <info@project-osrm.org>
2014-04-16 16:22:13 +0200

    reverse include order

--- Server/RequestHandler.cpp
+++ Server/RequestHandler.cpp
@@ -25,8 +25,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-#include "APIGrammar.h"
 #include "RequestHandler.h"
+#include "APIGrammar.h"
 
 #include "../Library/OSRM.h"
 #include "../Util/SimpleLogger.h"
