cfbc6b044118ea3ad7a10b13f9e884eacd582a74
Dennis Luxen <info@project-osrm.org>
2014-04-28 12:50:44 +0200

    remove useless parantheses

--- routed.cpp
+++ routed.cpp
@@ -89,7 +89,7 @@ int main (int argc, const char * argv[])
         }
 
 #ifdef __linux__
-        const int lock_flags = (MCL_CURRENT | MCL_FUTURE);
+        const int lock_flags = MCL_CURRENT | MCL_FUTURE;
         if (-1 == mlockall(lock_flags))
         {
             SimpleLogger().Write(logWARNING) << "Process " << argv[0] << " could not be locked to RAM";
