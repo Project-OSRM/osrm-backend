1b7a73ded7054de63e33ed508e301c04175de8a0
Emil Tin <emil@tin.dk>
2014-04-29 19:29:26 +0200

    test different status message and query parsing

--- features/step_definitions/routing.rb
+++ features/step_definitions/routing.rb
@@ -3,29 +3,36 @@ When /^I route I should get$/ do |table|
   actual = []
   OSRMBackgroundLauncher.new("#{@osm_file}.osrm") do
     table.hashes.each_with_index do |row,ri|
-      waypoints = []
-      if row['from'] and row['to']
-        node = find_node_by_name(row['from'])
-        raise "*** unknown from-node '#{row['from']}" unless node
-        waypoints << node
-
-        node = find_node_by_name(row['to'])
-        raise "*** unknown to-node '#{row['to']}" unless node
-        waypoints << node
-
-        got = {'from' => row['from'], 'to' => row['to'] }
-      elsif row['waypoints']
-        row['waypoints'].split(',').each do |n|
-          node = find_node_by_name(n.strip)
-          raise "*** unknown waypoint node '#{n.strip}" unless node
-          waypoints << node
-        end
-        got = {'waypoints' => row['waypoints'] }
+      if row['request']
+        got = {'request' => row['request'] }
+        response = request_url row['request']
       else
-        raise "*** no waypoints"
+        params = {}
+        waypoints = []
+        if row['from'] and row['to']
+          node = find_node_by_name(row['from'])
+          raise "*** unknown from-node '#{row['from']}" unless node
+          waypoints << node
+
+          node = find_node_by_name(row['to'])
+          raise "*** unknown to-node '#{row['to']}" unless node
+          waypoints << node
+
+          got = {'from' => row['from'], 'to' => row['to'] }
+          response = request_route waypoints, params
+        elsif row['waypoints']
+          row['waypoints'].split(',').each do |n|
+            node = find_node_by_name(n.strip)
+            raise "*** unknown waypoint node '#{n.strip}" unless node
+            waypoints << node
+          end
+          got = {'waypoints' => row['waypoints'] }
+          response = request_route waypoints, params
+        else
+          raise "*** no waypoints"
+        end
       end
 
-      params = {}
       row.each_pair do |k,v|
         if k =~ /param:(.*)/
           if v=='(nil)'
@@ -37,9 +44,11 @@ When /^I route I should get$/ do |table|
         end
       end
 
-      response = request_route(waypoints, params)
-      if response.code == "200" && response.body.empty? == false
+      if response.body.empty? == false
         json = JSON.parse response.body
+      end
+
+      if response.body.empty? == false
         if json['status'] == 0
           instructions = way_list json['route_instructions']
           bearings = bearing_list json['route_instructions']
@@ -51,61 +60,64 @@ When /^I route I should get$/ do |table|
         end
       end
 
-      if table.headers.include? 'start'
-        got['start'] = instructions ? json['route_summary']['start_point'] : nil
+      if table.headers.include? 'status'
+        got['status'] = json['status'].to_s
       end
-      if table.headers.include? 'end'
-        got['end'] = instructions ? json['route_summary']['end_point'] : nil
+      if table.headers.include? 'message'
+        got['message'] = json['status_message']
       end
-      if table.headers.include? 'route'
-        got['route'] = (instructions || '').strip
-        if table.headers.include?('distance')
-          if row['distance']!=''
-            raise "*** Distance must be specied in meters. (ex: 250m)" unless row['distance'] =~ /\d+m/
+      if table.headers.include? '#'   # comment column
+        got['#'] = row['#']           # copy value so it always match
+      end
+
+      if response.code == "200"
+        if table.headers.include? 'start'
+          got['start'] = instructions ? json['route_summary']['start_point'] : nil
+        end
+        if table.headers.include? 'end'
+          got['end'] = instructions ? json['route_summary']['end_point'] : nil
+        end
+        if table.headers.include? 'route'
+          got['route'] = (instructions || '').strip
+          if table.headers.include?('distance')
+            if row['distance']!=''
+              raise "*** Distance must be specied in meters. (ex: 250m)" unless row['distance'] =~ /\d+m/
+            end
+            got['distance'] = instructions ? "#{json['route_summary']['total_distance'].to_s}m" : ''
           end
-          got['distance'] = instructions ? "#{json['route_summary']['total_distance'].to_s}m" : ''
-        end
-        if table.headers.include?('time')
-          raise "*** Time must be specied in seconds. (ex: 60s)" unless row['time'] =~ /\d+s/
-          got['time'] = instructions ? "#{json['route_summary']['total_time'].to_s}s" : ''
-        end
-        if table.headers.include?('speed')
-          if row['speed'] != '' && instructions
-            raise "*** Speed must be specied in km/h. (ex: 50 km/h)" unless row['speed'] =~ /\d+ km\/h/
-              time = json['route_summary']['total_time']
-              distance = json['route_summary']['total_distance']
-              speed = time>0 ? (3.6*distance/time).to_i : nil
-              got['speed'] =  "#{speed} km/h"
-          else
-            got['speed'] = ''
+          if table.headers.include?('time')
+            raise "*** Time must be specied in seconds. (ex: 60s)" unless row['time'] =~ /\d+s/
+            got['time'] = instructions ? "#{json['route_summary']['total_time'].to_s}s" : ''
           end
-        end
-        if table.headers.include? 'bearing'
-          got['bearing'] = bearings
-        end
-        if table.headers.include? 'compass'
-          got['compass'] = compasses
-        end
-        if table.headers.include? 'turns'
-          got['turns'] = turns
-        end
-        if table.headers.include? 'modes'
-          got['modes'] = modes
-        end
-        if table.headers.include? 'times'
-          got['times'] = times
-        end
-        if table.headers.include? 'distances'
-          got['distances'] = distances
-        end
-        if table.headers.include? 'status'
-          got['status'] = json['status'].to_s
-        end
-        if table.headers.include? 'message'
-          got['message'] = json['status_message']
-        end
-        if table.headers.include? '#'   # comment column
-          got['#'] = row['#']           # copy value so it always match
+          if table.headers.include?('speed')
+            if row['speed'] != '' && instructions
+              raise "*** Speed must be specied in km/h. (ex: 50 km/h)" unless row['speed'] =~ /\d+ km\/h/
+                time = json['route_summary']['total_time']
+                distance = json['route_summary']['total_distance']
+                speed = time>0 ? (3.6*distance/time).to_i : nil
+                got['speed'] =  "#{speed} km/h"
+            else
+              got['speed'] = ''
+            end
+          end
+          if table.headers.include? 'bearing'
+            got['bearing'] = bearings
+          end
+          if table.headers.include? 'compass'
+            got['compass'] = compasses
+          end
+          if table.headers.include? 'turns'
+            got['turns'] = turns
+          end
+          if table.headers.include? 'modes'
+            got['modes'] = modes
+          end
+          if table.headers.include? 'times'
+            got['times'] = times
+          end
+          if table.headers.include? 'distances'
+            got['distances'] = distances
+          end
         end
       end
 
