380111a604bd47862a17da01a476eef0881a0d69
Dennis Luxen <info@project-osrm.org>
2014-05-05 18:21:03 +0200

    move Azimuth.h to C++11

--- Util/Azimuth.h
+++ Util/Azimuth.h
@@ -25,33 +25,49 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-#ifndef AZIMUTH_H_
-#define AZIMUTH_H_
+#ifndef AZIMUTH_H
+#define AZIMUTH_H
 
 #include <string>
 
-struct Azimuth {
-    static std::string Get(const double heading) {
-        if(heading <= 202.5) {
-            if(heading >= 0 && heading <= 22.5)
+struct Azimuth
+{
+    static std::string Get(const double heading)
+    {
+        if (heading <= 202.5)
+        {
+            if (heading >= 0 && heading <= 22.5)
+            {
                 return "N";
-            if(heading > 22.5 && heading <= 67.5)
+            }
+            if (heading > 22.5 && heading <= 67.5)
+            {
                 return "NE";
-            if(heading > 67.5 && heading <= 112.5)
+            }
+            if (heading > 67.5 && heading <= 112.5)
+            {
                 return "E";
-            if(heading > 112.5 && heading <= 157.5)
+            }
+            if (heading > 112.5 && heading <= 157.5)
+            {
                 return "SE";
+            }
             return "S";
         }
-        if(heading > 202.5 && heading <= 247.5)
+        if (heading > 202.5 && heading <= 247.5)
+        {
             return "SW";
-        if(heading > 247.5 && heading <= 292.5)
+        }
+        if (heading > 247.5 && heading <= 292.5)
+        {
             return "W";
-        if(heading > 292.5 && heading <= 337.5)
+        }
+        if (heading > 292.5 && heading <= 337.5)
+        {
             return "NW";
+        }
         return "N";
     }
 };
 
-
-#endif /* AZIMUTH_H_ */
+#endif // AZIMUTH_H
