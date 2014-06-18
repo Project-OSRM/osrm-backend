804e5158474d331b2cefe91695b8d26a1a0ca3e6
Dennis Luxen <info@project-osrm.org>
2014-06-18 11:25:56 +0200

    stream line code a bit in Reply

--- Server/Http/Reply.cpp
+++ Server/Http/Reply.cpp
@@ -90,7 +90,7 @@ Reply Reply::StockReply(Reply::status_type status)
     const std::string status_string = reply.ToString(status);
     reply.content.insert(reply.content.end(), status_string.begin(), status_string.end());
     reply.headers.emplace_back("Access-Control-Allow-Origin", "*");
-    reply.headers.emplace_back("Content-Length", UintToString(reply.content.size()));
+    reply.headers.emplace_back("Content-Length", UintToString(static_cast<unsigned>(reply.content.size())));
     reply.headers.emplace_back("Content-Type", "text/html");
     return reply;
 }
