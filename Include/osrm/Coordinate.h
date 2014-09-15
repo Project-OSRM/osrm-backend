02428f1e0695496d7ba4e7c779118890b0dbbd30
Dennis Luxen <info@project-osrm.org>
2014-09-15 12:21:13 +0200

    add object library to benchmarks

--- Include/osrm/Coordinate.h
+++ Include/osrm/Coordinate.h
@@ -31,7 +31,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <iosfwd> //for std::ostream
 #include <string>
 
-const float COORDINATE_PRECISION = 1000000.f;
+constexpr float COORDINATE_PRECISION = 1000000.f;
 
 struct FixedPointCoordinate
 {
