b29d5df7a21cacc94556681faddd46740af3e634
Dennis Luxen <info@project-osrm.org>
2014-10-15 10:43:58 +0200

    simplify conditional operation, remove useless parantheses

--- Util/simple_logger.cpp
+++ Util/simple_logger.cpp
@@ -114,7 +114,7 @@ SimpleLogger::~SimpleLogger()
     std::lock_guard<std::mutex> lock(get_mutex());
     if (!LogPolicy::GetInstance().IsMute())
     {
-        const bool is_terminal = (isatty(fileno(stdout)) ? false : true);
+        const bool is_terminal = isatty(fileno(stdout));
         switch (level)
         {
         case logWARNING:
