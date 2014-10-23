ae269e12bb216a1aa748637afa6ef29880ab7201
Dennis Luxen <info@project-osrm.org>
2014-10-23 16:11:35 +0200

    disable debug code in rtree

--- DataStructures/StaticRTree.h
+++ DataStructures/StaticRTree.h
@@ -655,13 +655,13 @@ class StaticRTree
         // SimpleLogger().Write(logDEBUG) << "searching for " << number_of_results << " results";
         std::vector<float> min_found_distances(number_of_results, std::numeric_limits<float>::max());
 
-        unsigned dequeues = 0;
-        unsigned inspected_mbrs = 0;
-        unsigned loaded_leafs = 0;
+        // unsigned dequeues = 0;
+        // unsigned inspected_mbrs = 0;
+        // unsigned loaded_leafs = 0;
         unsigned inspected_segments = 0;
-        unsigned pruned_elements = 0;
-        unsigned ignored_segments = 0;
-        unsigned ignored_mbrs = 0;
+        // unsigned pruned_elements = 0;
+        // unsigned ignored_segments = 0;
+        // unsigned ignored_mbrs = 0;
 
         unsigned number_of_results_found_in_big_cc = 0;
         unsigned number_of_results_found_in_tiny_cc = 0;
@@ -675,13 +675,13 @@ class StaticRTree
             const IncrementalQueryCandidate current_query_node = traversal_queue.top();
             traversal_queue.pop();
 
-            ++dequeues;
+            // ++dequeues;
 
             const float current_min_dist = min_found_distances[number_of_results-1];
 
             if (current_query_node.min_dist > current_min_dist)
             {
-                ++pruned_elements;
+                // ++pruned_elements;
                 continue;
             }
 
@@ -690,7 +690,7 @@ class StaticRTree
                 const TreeNode & current_tree_node = current_query_node.node.template get<TreeNode>();
                 if (current_tree_node.child_is_on_disk)
                 {
-                    ++loaded_leafs;
+                    // ++loaded_leafs;
                     // SimpleLogger().Write(logDEBUG) << "loading leaf: " << current_tree_node.children[0] << " w/ mbr [" <<
                     //     current_tree_node.minimum_bounding_rectangle.min_lat/COORDINATE_PRECISION << "," <<
                     //     current_tree_node.minimum_bounding_rectangle.min_lon/COORDINATE_PRECISION << "," <<
@@ -715,16 +715,16 @@ class StaticRTree
                         {
                             traversal_queue.emplace(current_perpendicular_distance, current_edge);
                         }
-                        else
-                        {
-                            ++ignored_segments;
-                        }
+                        // else
+                        // {
+                        //     ++ignored_segments;
+                        // }
                     }
                     // SimpleLogger().Write(logDEBUG) << "added " << current_leaf_node.object_count << " roads into queue of " << traversal_queue.size();
                 }
                 else
                 {
-                    ++inspected_mbrs;
+                    // ++inspected_mbrs;
                     // explore inner node
                     // SimpleLogger().Write(logDEBUG) << "explore inner node w/ mbr [" <<
                     //     current_tree_node.minimum_bounding_rectangle.min_lat/COORDINATE_PRECISION << "," <<
@@ -748,10 +748,10 @@ class StaticRTree
                         {
                             traversal_queue.emplace(lower_bound_to_element, child_tree_node);
                         }
-                        else
-                        {
-                            ++ignored_mbrs;
-                        }
+                        // else
+                        // {
+                        //     ++ignored_mbrs;
+                        // }
                     }
                     // SimpleLogger().Write(logDEBUG) << "added " << current_tree_node.child_count << " mbrs into queue of " << traversal_queue.size();
                 }
