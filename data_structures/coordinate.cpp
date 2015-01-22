796cbafde49bcdad1185eaff9b8bd7c7def47dcf
Dennis Luxen <info@project-osrm.org>
2015-01-22 17:57:38 +0100

    add missing include in release build

--- data_structures/coordinate.cpp
+++ data_structures/coordinate.cpp
@@ -39,6 +39,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef NDEBUG
 #include <bitset>
 #endif
+#include <iostream>
 #include <limits>
 
 FixedPointCoordinate::FixedPointCoordinate()
