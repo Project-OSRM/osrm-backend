38d7db49c88f5dd447bdd3b0a6269e88eae9dc5b
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2015-02-10 23:24:27 +0100

    Add state position to debug output

--- routing_algorithms/map_matching.hpp
+++ routing_algorithms/map_matching.hpp
@@ -279,6 +279,8 @@ template <class DataFacadeT> class MapMatching final
             {
                 JSON::Object _debug_state;
                 _debug_state.values["transitions"] = JSON::Array();
+                _debug_state.values["coordinate"] = makeJSONArray(l[i].first.location.lat / COORDINATE_PRECISION,
+                                                                  l[i].first.location.lon / COORDINATE_PRECISION);
                 _debug_timestamps.values.push_back(_debug_state);
             }
             _debug_states.values.push_back(_debug_timestamps);
