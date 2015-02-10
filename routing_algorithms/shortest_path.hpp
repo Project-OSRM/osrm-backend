b6314e0c73bd3fd572fd1e3229168f384af5f9d7
Dennis Luxen <info@project-osrm.org>
2015-02-10 12:27:27 +0100

    remove unnecessary casts in debug build

--- routing_algorithms/shortest_path.hpp
+++ routing_algorithms/shortest_path.hpp
@@ -236,8 +236,8 @@ class ShortestPathRouting final : public BasicRoutingInterface<DataFacadeT>
             std::vector<NodeID> temporary_packed_leg1;
             std::vector<NodeID> temporary_packed_leg2;
 
-            BOOST_ASSERT((unsigned)current_leg < packed_legs1.size());
-            BOOST_ASSERT((unsigned)current_leg < packed_legs2.size());
+            BOOST_ASSERT(current_leg < packed_legs1.size());
+            BOOST_ASSERT(current_leg < packed_legs2.size());
 
             if (INVALID_EDGE_WEIGHT != local_upper_bound1)
             {
