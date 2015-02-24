3da1e8598bbab477a235661051db911394fece12
Dennis Luxen <info@project-osrm.org>
2015-02-24 14:36:38 +0100

    fix copyright year

--- util/integer_range.hpp
+++ util/integer_range.hpp
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2013,2014, Project OSRM contributors
+Copyright (c) 2015, Project OSRM contributors
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
