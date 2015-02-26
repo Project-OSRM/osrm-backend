Project-OSRM/osrm-backend

commit fdf2e5934d40be6ce46ebea7dc0771e461758842

Add algorithm include for any_of

plugins/plugin_base.hpp
@@ -34,6 +34,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
 class BasePlugin
 {

commit 7cf363eb067bb6789c498de4c155dd7a7790fb8a

reorder includes, fixes style regression from commit fdf2e59.

plugins/plugin_base.hpp
@@ -32,9 +32,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <osrm/json_container.hpp>
 #include <osrm/route_parameters.hpp>
 
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 class BasePlugin
 {