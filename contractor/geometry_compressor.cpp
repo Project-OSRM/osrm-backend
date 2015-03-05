dd94a640488b4bd1573fd6fd0293b5de2119fce2
Dennis Luxen <info@project-osrm.org>
2015-03-05 14:03:35 +0100

    remove lint from #1409
    - fix copyright header
    - remove dead code

--- contractor/geometry_compressor.cpp
+++ contractor/geometry_compressor.cpp
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2015, Project OSRM, Dennis Luxen, others
+Copyright (c) 2015, Project OSRM contributors
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
@@ -34,8 +34,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <limits>
 #include <string>
-
-//int UniqueNumber() { return ++free_list_maximum; }
 
 GeometryCompressor::GeometryCompressor()
 {
