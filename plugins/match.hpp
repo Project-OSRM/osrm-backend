0c735953c9408fe69fe3da64c983ea78e85a744c
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2015-03-08 00:51:07 +0100

    Make uturn detection a little less sensitive.

--- plugins/match.hpp
+++ plugins/match.hpp
@@ -124,7 +124,7 @@ template <class DataFacadeT> class MapMatchingPlugin : public BasePlugin
                     input_coords[current_coordinate + 1]);
 
                 // sharp turns indicate a possible uturn
-                if (turn_angle < 100.0 || turn_angle > 260.0)
+                if (turn_angle <= 90.0 || turn_angle >= 270.0)
                 {
                     allow_uturn = true;
                 }
