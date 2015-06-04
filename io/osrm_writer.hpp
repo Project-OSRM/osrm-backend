39383855dd6958a0de3b9981b2780b4a37ce4b56
Daniel J. Hofmann <daniel@trvx.org>
2015-06-05 00:24:02 +0200

    Make header policies explicit in OSRMWriter abstraction and hint for inlining

--- io/osrm_writer.hpp
+++ io/osrm_writer.hpp
@@ -18,6 +18,8 @@
 //
 //  As of now the policies as stateless; this may change though
 //  in order to support e.g. profiling policies that hold timers.
+//
+//  Note: see end of file for HeaderWriter, EdgeWriter, NodeWriter.
 
 template <typename HeaderWritePolicy, typename TypeWritePolicy, typename FinalizeWritePolicy>
 class OSRMWriter final
@@ -28,16 +30,12 @@ class OSRMWriter final
         : stream_{stream}, segment_start_(stream_.tellp()), count_{0}
     {
         header_offset_ = HeaderWritePolicy::Write(header, stream_, segment_start_, count_);
-
-        unsigned reserve_prefix = 0;
-        const auto written =
-            TypeWritePolicy::Write(reserve_prefix, stream_, segment_start_, header_offset_, count_);
-        (void)written; // unused, as this is reserved space
     }
 
     ~OSRMWriter()
     {
-        const auto len = FinalizeWritePolicy::Write(stream_, segment_start_, header_offset_, count_);
+        const auto len =
+            FinalizeWritePolicy::Write(stream_, segment_start_, header_offset_, count_);
         (void)len; // unused
     }
 
@@ -61,7 +59,7 @@ class OSRMWriter final
 struct NoHeaderWritePolicy final
 {
     template <typename T>
-    static std::size_t Write(const T &, std::ostream &, std::size_t, std::size_t)
+    static inline std::size_t Write(const T &, std::ostream &, std::size_t, std::size_t)
     {
         return 0;
     }
@@ -70,7 +68,8 @@ struct NoHeaderWritePolicy final
 struct NoTypeWritePolicy final
 {
     template <typename T>
-    static std::size_t Write(const T &, std::ostream &, std::size_t, std::size_t, std::size_t)
+    static inline std::size_t
+    Write(const T &, std::ostream &, std::size_t, std::size_t, std::size_t)
     {
         return 0;
     }
@@ -78,7 +77,10 @@ struct NoTypeWritePolicy final
 
 struct NoFinalizeWritePolicy final
 {
-    static std::size_t Write(std::ostream &, std::size_t, std::size_t, std::size_t) { return 0; }
+    static inline std::size_t Write(std::ostream &, std::size_t, std::size_t, std::size_t)
+    {
+        return 0;
+    }
 };
 
 // TODO: Debug Policies, i.e. diagnostics to stderr
@@ -87,12 +89,11 @@ struct NoFinalizeWritePolicy final
 struct TrivialHeaderWritePolicy final
 {
     template <typename T>
-    static std::size_t
-    Write(const T &header, std::ostream &stream, std::size_t segment_start, std::size_t count)
+    static inline std::size_t Write(const T &header, std::ostream &stream, std::size_t, std::size_t)
     {
         // TODO: strictly speaking we need a trivial type, but most are not; check this on callside
         // static_assert(std::is_trivial<T>::value, "T is not a trivial type");
-        const auto offset = sizeof(T);
+        const constexpr auto offset = sizeof(T);
         stream.write(reinterpret_cast<const char *>(&header), offset);
         return offset;
     }
@@ -101,38 +102,55 @@ struct TrivialHeaderWritePolicy final
 struct TrivialTypeWritePolicy final
 {
     template <typename T>
-    static std::size_t Write(const T &item,
-                             std::ostream &stream,
-                             std::size_t segment_start,
-                             std::size_t header_off,
-                             std::size_t count)
+    static inline std::size_t
+    Write(const T &item, std::ostream &stream, std::size_t, std::size_t, std::size_t)
     {
         // TODO: strictly speaking we need a trivial type, but most are not; check this on callside
         // static_assert(std::is_trivial<T>::value, "T is not a trivial type");
         stream.write(reinterpret_cast<const char *>(&item), sizeof(decltype(item)));
-        return 1u;
+        return 1;
+    }
+};
+
+struct LengthPrefixHeaderWritePolicy final
+{
+    template <typename T>
+    static inline std::size_t Write(const T &, std::ostream &stream, std::size_t, std::size_t)
+    {
+        // XXX: why do we write unsigned; what about overflow?
+        const unsigned reserved = 0;
+        stream.write(reinterpret_cast<const char *>(&reserved), sizeof(decltype(reserved)));
+        return 0; // do not offset the reserved space as finalizer writes into it
     }
 };
 
 struct LengthPrefixFinalizeWritePolicy final
 {
-    static std::size_t Write(std::ostream &stream,
-                             std::size_t segment_start,
-                             std::size_t header_offset,
-                             std::size_t count)
+    static inline std::size_t Write(std::ostream &stream,
+                                    std::size_t segment_start,
+                                    std::size_t header_offset,
+                                    std::size_t count)
     {
         const auto here = stream.tellp();
         stream.seekp(segment_start + header_offset);
         // XXX: why do we write unsigned; what about overflow?
-        unsigned len = static_cast<unsigned>(count);
+        const auto len = static_cast<unsigned>(count);
         stream.write(reinterpret_cast<const char *>(&len), sizeof(decltype(len)));
         stream.seekp(here);
-        return 1u;
+        return 1;
     }
 };
 
-using HeaderWriter = OSRMWriter<TrivialHeaderWritePolicy, NoTypeWritePolicy, NoFinalizeWritePolicy>;
-using EdgeWriter = OSRMWriter<NoHeaderWritePolicy, TrivialTypeWritePolicy, LengthPrefixFinalizeWritePolicy>;
-using NodeWriter = OSRMWriter<NoHeaderWritePolicy, TrivialTypeWritePolicy, LengthPrefixFinalizeWritePolicy>;
+using HeaderWriter = OSRMWriter<TrivialHeaderWritePolicy, // Write headers of trivial type
+                                NoTypeWritePolicy,        // No elements to write
+                                NoFinalizeWritePolicy>;   // No finalizer
+
+using EdgeWriter = OSRMWriter<LengthPrefixHeaderWritePolicy,    // Reserve for length
+                              TrivialTypeWritePolicy,           // Write items of trivial type
+                              LengthPrefixFinalizeWritePolicy>; // Write length into reserved
+
+using NodeWriter = OSRMWriter<LengthPrefixHeaderWritePolicy,    // Reserve for length
+                              TrivialTypeWritePolicy,           // Write items of trivial type
+                              LengthPrefixFinalizeWritePolicy>; // Write length into reserved
 
 #endif
