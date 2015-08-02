43b881d0cd5259e47e789f9ae92c90f4ea536b70
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2015-08-02 14:45:20 +0200

    Simplify test.sh

--- docker/test.sh
+++ docker/test.sh
@@ -3,12 +3,8 @@
 set -e
 set -o pipefail
 
-export TRAVIS_OS_NAME=linux
-export CMAKEOPTIONS="-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-4.8"
-export OSRM_PORT=5000
-export OSRM_TIMEOUT=60
+export CMAKEOPTIONS="-DCMAKE_BUILD_TYPE=Release"
 export PATH=$PATH:/home/mapbox/.gem/ruby/1.9.1/bin:/home/mapbox/osrm-backend/vendor/bundle/ruby/1.9.1/bin
-export BRANCH=develop
 
 cd /home/mapbox/osrm-backend
 gem install --user-install bundler
