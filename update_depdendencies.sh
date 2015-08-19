3d84dbc73fe808327db40f6afb9e51cc532db7e4
Daniel J. Hofmann <daniel@trvx.org>
2015-08-19 20:20:43 +0200

    Check for releases and request user confirmation before updating subtrees

--- update_depdendencies.sh
+++ update_depdendencies.sh
@@ -6,6 +6,16 @@ OSMIUM_TAG=v2.3.0
 VARIANT_REPO=https://github.com/mapbox/variant.git 
 VARIANT_TAG=v1.0
 
+VARIANT_LATEST=$(http --body https://api.github.com/repos/mapbox/variant/releases/latest | jq ".tag_name")
+OSMIUM_LATEST=$(http --body https://api.github.com/repos/osmcode/libosmium/releases/latest | jq ".tag_name")
 
-git subtree pull -P third_party/libosmium/ $OSMIUM_REPO $OSMIUM_TAG --squash
-git subtree pull -P third_party/variant/ $VARIANT_REPO $VARIANT_TAG --squash
+echo "Latest osmium release is $OSMIUM_LATEST, pulling in \"$OSMIUM_TAG\""
+echo "Latest variant release is $VARIANT_LATEST, pulling in \"$VARIANT_TAG\""
+
+read -p "Looks good? (Y/n) " ok
+
+if [[ $ok =~ [yY] ]]
+then
+  echo git subtree pull -P third_party/libosmium/ $OSMIUM_REPO $OSMIUM_TAG --squash
+  echo git subtree pull -P third_party/variant/ $VARIANT_REPO $VARIANT_TAG --squash
+fi
