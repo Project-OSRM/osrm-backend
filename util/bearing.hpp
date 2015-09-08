98b7e0a4072d51b06aaa90e7185b367adeeab6af
Daniel J. Hofmann <daniel@trvx.org>
2015-09-09 01:40:01 +0200

    Refactor bearing implementation.
    - removes `noexcept` specifier as we can not guarantee for not throwing
    
    - uses a namespace instead of a struct + static function combination
    
    - asserts for heading degree in [0, 360] range (both sides inclusive!)
    
    - header only since implementation does not hide anything
    
    - adds `inline` specifier as compiler hint

--- util/bearing.hpp
+++ util/bearing.hpp
@@ -28,11 +28,50 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef BEARING_HPP
 #define BEARING_HPP
 
+#include <boost/assert.hpp>
 #include <string>
 
-struct bearing
+namespace bearing
 {
-    static std::string get(const double heading) noexcept;
-};
+inline std::string get(const double heading)
+{
+    BOOST_ASSERT(heading >= 0);
+    BOOST_ASSERT(heading <= 360);
+
+    if (heading <= 22.5)
+    {
+        return "N";
+    }
+    if (heading <= 67.5)
+    {
+        return "NE";
+    }
+    if (heading <= 112.5)
+    {
+        return "E";
+    }
+    if (heading <= 157.5)
+    {
+        return "SE";
+    }
+    if (heading <= 202.5)
+    {
+        return "S";
+    }
+    if (heading <= 247.5)
+    {
+        return "SW";
+    }
+    if (heading <= 292.5)
+    {
+        return "W";
+    }
+    if (heading <= 337.5)
+    {
+        return "NW";
+    }
+    return "N";
+}
+}
 
 #endif // BEARING_HPP
