6c35272fe2f8cc4df5a2606ee45314f222861c9e
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2015-11-18 18:06:50 +0100

    Remove obsolte code as we use a turn function now

--- profiles/car.lua
+++ profiles/car.lua
@@ -181,15 +181,6 @@ local function parse_maxspeed(source)
   return n
 end
 
--- FIXME Why was this commented out?
--- function turn_function (angle)
---   -- print ("called at angle " .. angle )
---   local index = math.abs(math.floor(angle/10+0.5))+1 -- +1 'coz LUA starts as idx 1
---   local penalty = turn_cost_table[index]
---   -- print ("index: " .. index .. ", bias: " .. penalty )
---   return penalty
--- end
-
 function node_function (node, result)
   -- parse access and barrier tags
   local access = find_access_tag(node, access_tags_hierachy)
