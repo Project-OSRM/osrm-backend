f8b5e7e2c9d726c77842a3aea460dcfc7bc903fe
Daniel J. Hofmann <daniel@trvx.org>
2016-01-21 15:58:14 +0100

    Removes XORMiniHash, unused

--- include/util/xor_fast_hash.hpp
+++ include/util/xor_fast_hash.hpp
@@ -61,42 +61,6 @@ class XORFastHash
         return table1[lsb] ^ table2[msb];
     }
 };
-
-class XORMiniHash
-{ // 256 entries
-    std::vector<unsigned char> table1;
-    std::vector<unsigned char> table2;
-    std::vector<unsigned char> table3;
-    std::vector<unsigned char> table4;
-
-  public:
-    XORMiniHash()
-    {
-        table1.resize(1 << 8);
-        table2.resize(1 << 8);
-        table3.resize(1 << 8);
-        table4.resize(1 << 8);
-        for (unsigned i = 0; i < (1 << 8); ++i)
-        {
-            table1[i] = static_cast<unsigned char>(i);
-            table2[i] = static_cast<unsigned char>(i);
-            table3[i] = static_cast<unsigned char>(i);
-            table4[i] = static_cast<unsigned char>(i);
-        }
-        std::random_shuffle(table1.begin(), table1.end());
-        std::random_shuffle(table2.begin(), table2.end());
-        std::random_shuffle(table3.begin(), table3.end());
-        std::random_shuffle(table4.begin(), table4.end());
-    }
-    unsigned char operator()(const unsigned originalValue) const
-    {
-        unsigned char byte1 = ((originalValue)&0xff);
-        unsigned char byte2 = ((originalValue >> 8) & 0xff);
-        unsigned char byte3 = ((originalValue >> 16) & 0xff);
-        unsigned char byte4 = ((originalValue >> 24) & 0xff);
-        return table1[byte1] ^ table2[byte2] ^ table3[byte3] ^ table4[byte4];
-    }
-};
 }
 }
 
