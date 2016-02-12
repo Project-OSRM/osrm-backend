d1c4a2679105616146ec7d80bfa76340d34ccf22
Daniel J. Hofmann <daniel@trvx.org>
2016-02-11 19:19:28 -0800

    Remove .restriction reference

--- src/tools/extract.cpp
+++ src/tools/extract.cpp
@@ -28,12 +28,6 @@ parseArguments(int argc, char *argv[], extractor::ExtractorConfig &extractor_con
     // declare a group of options that will be allowed only on command line
     boost::program_options::options_description generic_options("Options");
     generic_options.add_options()("version,v", "Show version")("help,h", "Show this help message")(
-        /*
-         * TODO: re-enable this
-        "restrictions,r",
-        boost::program_options::value<boost::filesystem::path>(&extractor_config.restrictions_path),
-        "Restrictions file in .osrm.restrictions format")(
-        */
         "config,c",
         boost::program_options::value<boost::filesystem::path>(&extractor_config.config_file_path)
             ->default_value("extractor.ini"),
