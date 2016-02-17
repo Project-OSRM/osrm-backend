a4074332ccf67ccec9f27d0bb561c0f028f7b132
Daniel J. Hofmann <daniel@trvx.org>
2016-02-17 12:56:27 -0800

    Adapts publicly facing new API

--- src/osrm/osrm.cpp
+++ src/osrm/osrm.cpp
@@ -1,6 +1,9 @@
 #include "osrm/osrm.hpp"
 #include "engine/api/route_parameters.hpp"
 #include "engine/api/table_parameters.hpp"
+#include "engine/api/nearest_parameters.hpp"
+//#include "engine/api/trip_parameters.hpp"
+//#include "engine/api/match_parameters.hpp"
 #include "engine/engine.hpp"
 #include "engine/status.hpp"
 #include "engine/engine_config.hpp"
@@ -27,5 +30,20 @@ engine::Status OSRM::Table(const engine::api::TableParameters &params, json::Obj
 {
     return engine_->Table(params, result);
 }
+
+engine::Status OSRM::Nearest(const engine::api::NearestParameters &params, json::Object &result)
+{
+    return engine_->Nearest(params, result);
+}
+
+//engine::Status OSRM::Trip(const engine::api::TripParameters &params, json::Object &result)
+//{
+//    return engine_->Trip(params, result);
+//}
+//
+//engine::Status OSRM::Match(const engine::api::MatchParameters &params, json::Object &result)
+//{
+//    return engine_->Match(params, result);
+//}
 
 } // ns osrm
