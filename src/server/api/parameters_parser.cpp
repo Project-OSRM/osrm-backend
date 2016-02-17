6fecce23fc5a6af5cb9df1024405eac590f13ba5
Daniel J. Hofmann <daniel@trvx.org>
2016-02-17 15:06:52 -0800

    Link parameters to grammars

--- src/server/api/parameters_parser.cpp
+++ src/server/api/parameters_parser.cpp
@@ -2,6 +2,9 @@
 
 #include "server/api/route_parameters_grammar.hpp"
 #include "server/api/table_parameter_grammar.hpp"
+#include "server/api/nearest_parameter_grammar.hpp"
+//#include "server/api/trip_parameter_grammar.hpp"
+//#include "server/api/match_parameter_grammar.hpp"
 
 #include <type_traits>
 
@@ -42,6 +45,28 @@ boost::optional<engine::api::TableParameters> parseParameters(std::string::itera
 {
     return detail::parseParameters<engine::api::TableParameters, TableParametersGrammar>(iter, end);
 }
+
+template <>
+boost::optional<engine::api::NearestParameters> parseParameters(std::string::iterator &iter,
+                                                                std::string::iterator end)
+{
+    return detail::parseParameters<engine::api::NearestParameters, NearestParametersGrammar>(iter,
+                                                                                             end);
+}
+
+//template <>
+//boost::optional<engine::api::TripParameters> parseParameters(std::string::iterator &iter,
+//                                                             std::string::iterator end)
+//{
+//    return detail::parseParameters<engine::api::TripParameters, TripParametersGrammar>(iter, end);
+//}
+//
+//template <>
+//boost::optional<engine::api::MatchParameters> parseParameters(std::string::iterator &iter,
+//                                                              std::string::iterator end)
+//{
+//    return detail::parseParameters<engine::api::MatchParameters, MatchParametersGrammar>(iter, end);
+//}
 
 } // ns api
 } // ns server
