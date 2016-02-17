0c645032180ceb8a1acb620167ad16ff8ceed3d5
Daniel J. Hofmann <daniel@trvx.org>
2016-02-17 15:49:10 -0800

    Service skeletons for nearest, trip, match

--- include/server/service/table_service.hpp
+++ include/server/service/table_service.hpp
@@ -20,12 +20,13 @@ namespace service
 class TableService final : public BaseService
 {
   public:
-    TableService(OSRM& routing_machine) : BaseService(routing_machine) {}
+    TableService(OSRM &routing_machine) : BaseService(routing_machine) {}
 
-    virtual engine::Status RunQuery(std::vector<util::FixedPointCoordinate> coordinates,
-                                    std::string &options,
-                                    util::json::Object &json_result) final override;
-    virtual unsigned GetVersion() final override { return 1; }
+    engine::Status RunQuery(std::vector<util::FixedPointCoordinate> coordinates,
+                            std::string &options,
+                            util::json::Object &result) final override;
+
+    unsigned GetVersion() final override { return 1; }
 };
 }
 }
