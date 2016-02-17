49a173652bd8f51270f3eba888e045e721d7a681
Daniel J. Hofmann <daniel@trvx.org>
2016-02-17 15:49:10 -0800

    Service skeletons for nearest, trip, match

---
+++ src/server/service/nearest_service.cpp
@@ -0,0 +1,26 @@
+#include "server/service/nearest_service.hpp"
+
+#include "engine/api/nearest_parameters.hpp"
+#include "server/api/parameters_parser.hpp"
+
+#include "util/json_container.hpp"
+
+#include <boost/format.hpp>
+
+namespace osrm
+{
+namespace server
+{
+namespace service
+{
+
+engine::Status RouteService::RunQuery(std::vector<util::FixedPointCoordinate> coordinates,
+                                      std::string &options,
+                                      util::json::Object &json_result)
+{
+    // TODO(daniel-j-h)
+    return Status::Error;
+}
+}
+}
+}
