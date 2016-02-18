bbf18002a8477048ab58462e2c34d7be62ea7ed5
Daniel J. Hofmann <daniel@trvx.org>
2016-02-18 12:21:03 -0800

    Adds publicly facing alias headers for parameters

---
+++ include/osrm/match_parameters.hpp
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_MATCH_PARAMETERS_HPP
+#define GLOBAL_MATCH_PARAMETERS_HPP
+
+#include "engine/api/match_parameters.hpp"
+
+namespace osrm
+{
+    using engine::api::MatchParameters;
+}
+
+#endif
