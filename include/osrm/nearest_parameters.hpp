9578b9a045de1111ec023c9844f8e0bd04908c6f
Daniel J. Hofmann <daniel@trvx.org>
2016-02-18 12:21:03 -0800

    Adds publicly facing alias headers for parameters

---
+++ include/osrm/nearest_parameters.hpp
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_NEAREST_PARAMETERS_HPP
+#define GLOBAL_NEAREST_PARAMETERS_HPP
+
+#include "engine/api/nearest_parameters.hpp"
+
+namespace osrm
+{
+    using engine::api::NearestParameters;
+}
+
+#endif
---
+++ include/osrm/table_parameters.hpp
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_TABLE_PARAMETERS_HPP
+#define GLOBAL_TABLE_PARAMETERS_HPP
+
+#include "engine/api/table_parameters.hpp"
+
+namespace osrm
+{
+    using engine::api::TableParameters;
+}
+
+#endif
