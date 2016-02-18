99f18051f6ddfec16dbfaae657c4c4dc0ecd2055
Daniel J. Hofmann <daniel@trvx.org>
2016-02-18 12:21:03 -0800

    Adds publicly facing alias headers for parameters

---
+++ include/osrm/trip_parameters.hpp
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_TRIP_PARAMETERS_HPP
+#define GLOBAL_TRIP_PARAMETERS_HPP
+
+#include "engine/api/trip_parameters.hpp"
+
+namespace osrm
+{
+    using engine::api::TripParameters;
+}
+
+#endif
