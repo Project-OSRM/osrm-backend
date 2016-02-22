089e012d152ea4fa29c40134003963d060dc571a
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-02-22 17:33:31 +0100

    Hook up map matching

--- include/engine/api/match_parameters.hpp
+++ include/engine/api/match_parameters.hpp
@@ -14,6 +14,18 @@ namespace api
 
 struct MatchParameters : public RouteParameters
 {
+    MatchParameters()
+        : RouteParameters(false, false, RouteParameters::GeometriesType::Polyline, RouteParameters::OverviewType::Simplified, {})
+    {
+    }
+
+    template<typename... Args>
+    MatchParameters(std::vector<unsigned> timestamps_,
+                    Args... args_)
+        : RouteParameters{std::forward<Args>(args_)...}, timestamps{std::move(timestamps_)}
+    {
+    }
+
     std::vector<unsigned> timestamps;
     bool IsValid() const
     {
