c19fae4a4375ebd5a627828d65e6c3c28c2a6caf
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-03-01 22:30:31 +0100

    Big Restructuring / Cleanup

--- src/storage/storage.cpp
+++ src/storage/storage.cpp
@@ -8,7 +8,7 @@
 #include "util/static_rtree.hpp"
 #include "engine/datafacade/datafacade_base.hpp"
 #include "extractor/travel_mode.hpp"
-#include "engine/guidance/turn_instruction.hpp"
+#include "extractor/guidance/turn_instruction.hpp"
 #include "storage/storage.hpp"
 #include "storage/shared_datatype.hpp"
 #include "storage/shared_barriers.hpp"
@@ -225,7 +225,7 @@ int Storage::Run()
                                               number_of_original_edges);
     shared_layout_ptr->SetBlockSize<extractor::TravelMode>(SharedDataLayout::TRAVEL_MODE,
                                                            number_of_original_edges);
-    shared_layout_ptr->SetBlockSize<engine::guidance::TurnInstruction>(
+    shared_layout_ptr->SetBlockSize<extractor::guidance::TurnInstruction>(
         SharedDataLayout::TURN_INSTRUCTION, number_of_original_edges);
 
     boost::filesystem::ifstream hsgr_input_stream(hsgr_path, std::ios::binary);
@@ -390,8 +390,8 @@ int Storage::Run()
         shared_layout_ptr->GetBlockPtr<extractor::TravelMode, true>(shared_memory_ptr,
                                                                     SharedDataLayout::TRAVEL_MODE);
 
-    engine::guidance::TurnInstruction *turn_instructions_ptr =
-        shared_layout_ptr->GetBlockPtr<engine::guidance::TurnInstruction, true>(
+    extractor::guidance::TurnInstruction *turn_instructions_ptr =
+        shared_layout_ptr->GetBlockPtr<extractor::guidance::TurnInstruction, true>(
             shared_memory_ptr, SharedDataLayout::TURN_INSTRUCTION);
 
     extractor::OriginalEdgeData current_edge_data;
