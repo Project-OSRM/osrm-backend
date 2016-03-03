a1e4d5b0a38e45f829af33d0d7c6d5a0161a22bc
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-03-03 01:48:30 +0100

    Add support for tile plugin

--- src/server/service_handler.cpp
+++ src/server/service_handler.cpp
@@ -5,6 +5,7 @@
 #include "server/service/nearest_service.hpp"
 #include "server/service/trip_service.hpp"
 #include "server/service/match_service.hpp"
+#include "server/service/tile_service.hpp"
 
 #include "server/api/parsed_url.hpp"
 #include "util/json_util.hpp"
@@ -21,27 +22,32 @@ ServiceHandler::ServiceHandler(osrm::EngineConfig &config) : routing_machine(con
     service_map["nearest"] = util::make_unique<service::NearestService>(routing_machine);
     service_map["trip"] = util::make_unique<service::TripService>(routing_machine);
     service_map["match"] = util::make_unique<service::MatchService>(routing_machine);
+    service_map["tile"] = util::make_unique<service::TileService>(routing_machine);
 }
 
-engine::Status ServiceHandler::RunQuery(api::ParsedURL parsed_url, util::json::Object &result)
+engine::Status ServiceHandler::RunQuery(api::ParsedURL parsed_url, service::BaseService::ResultT &result)
 {
     const auto &service_iter = service_map.find(parsed_url.service);
     if (service_iter == service_map.end())
     {
-        result.values["code"] = "invalid-service";
-        result.values["message"] = "Service " + parsed_url.service + " not found!";
+        result = util::json::Object();
+        auto &json_result = result.get<util::json::Object>();
+        json_result.values["code"] = "invalid-service";
+        json_result.values["message"] = "Service " + parsed_url.service + " not found!";
         return engine::Status::Error;
     }
     auto &service = service_iter->second;
 
     if (service->GetVersion() != parsed_url.version)
     {
-        result.values["code"] = "invalid-version";
-        result.values["message"] = "Service " + parsed_url.service + " not found!";
+        result = util::json::Object();
+        auto &json_result = result.get<util::json::Object>();
+        json_result.values["code"] = "invalid-version";
+        json_result.values["message"] = "Service " + parsed_url.service + " not found!";
         return engine::Status::Error;
     }
 
-    return service->RunQuery(std::move(parsed_url.coordinates), parsed_url.options, result);
+    return service->RunQuery(parsed_url.query, result);
 }
 }
 }
