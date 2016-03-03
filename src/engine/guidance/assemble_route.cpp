e1da384b62009fb400832a0a4c3f67213634b1c2
Daniel J. Hofmann <daniel@trvx.org>
2016-03-03 15:59:02 +0100

    Moves route assembly into implementation file

---
+++ src/engine/guidance/assemble_route.cpp
@@ -0,0 +1,30 @@
+#include "engine/guidance/assemble_route.hpp"
+
+#include <numeric>
+
+namespace osrm
+{
+namespace engine
+{
+namespace guidance
+{
+
+Route assembleRoute(const std::vector<RouteLeg> &route_legs)
+{
+    auto distance = std::accumulate(route_legs.begin(), route_legs.end(), 0.,
+                                    [](const double sum, const RouteLeg &leg)
+                                    {
+                                        return sum + leg.distance;
+                                    });
+    auto duration = std::accumulate(route_legs.begin(), route_legs.end(), 0.,
+                                    [](const double sum, const RouteLeg &leg)
+                                    {
+                                        return sum + leg.duration;
+                                    });
+
+    return Route{duration, distance};
+}
+
+} // namespace guidance
+} // namespace engine
+} // namespace osrm
