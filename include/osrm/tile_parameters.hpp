a8fc95d4e43b1951e72824be2b83622646349a1c
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-03-04 23:17:57 +0100

    Move bearing to public namespace

---
+++ include/osrm/tile_parameters.hpp
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_TILE_PARAMETERS_HPP
+#define GLOBAL_TILE_PARAMETERS_HPP
+
+#include "engine/api/tile_parameters.hpp"
+
+namespace osrm
+{
+using engine::api::TileParameters;
+}
+
+#endif
