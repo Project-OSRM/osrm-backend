6c9ddde682f1fe7b6e4f9c6df2864b74d6d8c1a9
Daniel J. Hofmann <daniel@trvx.org>
2016-03-14 15:25:36 +0100

    RFC 4648 Test Vectors

--- unit_tests/engine/base64.cpp
+++ unit_tests/engine/base64.cpp
@@ -3,7 +3,22 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/test_case_template.hpp>
 
+// RFC 4648 "The Base16, Base32, and Base64 Data Encodings"
 BOOST_AUTO_TEST_SUITE(base64)
+
+// For test vectors see section 10: https://tools.ietf.org/html/rfc4648#section-10
+BOOST_AUTO_TEST_CASE(rfc4648_test_vectors)
+{
+    using namespace osrm::engine;
+
+    BOOST_CHECK_EQUAL(encodeBase64(""), "");
+    BOOST_CHECK_EQUAL(encodeBase64("f"), "Zg==");
+    BOOST_CHECK_EQUAL(encodeBase64("fo"), "Zm8=");
+    BOOST_CHECK_EQUAL(encodeBase64("foo"), "Zm9v");
+    BOOST_CHECK_EQUAL(encodeBase64("foob"), "Zm9vYg==");
+    BOOST_CHECK_EQUAL(encodeBase64("fooba"), "Zm9vYmE=");
+    BOOST_CHECK_EQUAL(encodeBase64("foobar"), "Zm9vYmFy");
+}
 
 BOOST_AUTO_TEST_CASE(encoding) {}
 
