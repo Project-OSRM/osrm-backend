5490294c037e34a13711712896c91ed8f0a384f6
Daniel J. Hofmann <daniel@trvx.org>
2016-03-16 11:58:39 +0100

    Library tests build system integration

---
+++ unit_tests/library/match.cpp
@@ -0,0 +1,8 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_case_template.hpp>
+
+BOOST_AUTO_TEST_SUITE(match)
+
+BOOST_AUTO_TEST_CASE(test_match) {}
+
+BOOST_AUTO_TEST_SUITE_END()
