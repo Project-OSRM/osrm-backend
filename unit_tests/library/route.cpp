9d893d6d4d18ba44515b5f6afc7e0347e6d486ef
Daniel J. Hofmann <daniel@trvx.org>
2016-03-16 11:58:39 +0100

    Library tests build system integration

---
+++ unit_tests/library/route.cpp
@@ -0,0 +1,8 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_case_template.hpp>
+
+BOOST_AUTO_TEST_SUITE(route)
+
+BOOST_AUTO_TEST_CASE(test_route) {}
+
+BOOST_AUTO_TEST_SUITE_END()
