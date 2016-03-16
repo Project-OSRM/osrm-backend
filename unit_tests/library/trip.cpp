8f950f059e7130b1c4a5705fede93f6a4490d98f
Daniel J. Hofmann <daniel@trvx.org>
2016-03-16 12:42:26 +0100

    Initial unit tests setup for typed libosrm services

--- unit_tests/library/trip.cpp
+++ unit_tests/library/trip.cpp
@@ -1,8 +1,39 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/test_case_template.hpp>
 
+#include "args.h"
+
+#include "osrm/trip_parameters.hpp"
+
+#include "osrm/coordinate.hpp"
+#include "osrm/engine_config.hpp"
+#include "osrm/json_container.hpp"
+#include "osrm/status.hpp"
+#include "osrm/osrm.hpp"
+
 BOOST_AUTO_TEST_SUITE(trip)
 
-BOOST_AUTO_TEST_CASE(test_trip) {}
+BOOST_AUTO_TEST_CASE(test_trip)
+{
+    const auto args = get_args();
+    BOOST_REQUIRE_EQUAL(args.size(), 1);
+
+    using namespace osrm;
+
+    EngineConfig config{args[0]};
+    config.use_shared_memory = false;
+
+    OSRM osrm{config};
+
+    /*
+    TripParameters params;
+
+    json::Object result;
+
+    const auto rc = osrm.Trip(params, result);
+
+    BOOST_CHECK(rc == Status::Ok || rc == Status::Error);
+    */
+}
 
 BOOST_AUTO_TEST_SUITE_END()
