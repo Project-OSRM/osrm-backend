6b768a7da7f4c4f500505481220cd70335a58b30
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-03-18 19:21:13 +0100

    get_name_for_id -> GetNameForID

--- include/engine/datafacade/shared_datafacade.hpp
+++ include/engine/datafacade/shared_datafacade.hpp
@@ -621,7 +621,7 @@ class SharedDataFacade final : public BaseDataFacade
         return m_name_ID_list.at(id);
     }
 
-    std::string get_name_for_id(const unsigned name_id) const override final
+    std::string GetNameForID(const unsigned name_id) const override final
     {
         if (std::numeric_limits<unsigned>::max() == name_id)
         {
