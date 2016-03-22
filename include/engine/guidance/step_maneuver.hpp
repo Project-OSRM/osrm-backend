80111003177ef20b944a7d3f1ecc3e5311fb2e8b
Moritz Kobitzsch <moritz@mapbox.com>
2016-03-22 14:17:17 +0100

    added list of intersections to the step-maneuver, not in api so far

--- include/engine/guidance/step_maneuver.hpp
+++ include/engine/guidance/step_maneuver.hpp
@@ -5,6 +5,7 @@
 #include "extractor/guidance/turn_instruction.hpp"
 
 #include <cstdint>
+#include <vector>
 
 namespace osrm
 {
@@ -20,6 +21,14 @@ enum class WaypointType : std::uint8_t
     Depart,
 };
 
+//A represenetation of intermediate intersections
+struct IntermediateIntersection
+{
+    double duration;
+    double distance;
+    util::Coordinate location;
+};
+
 struct StepManeuver
 {
     util::Coordinate location;
@@ -28,7 +37,7 @@ struct StepManeuver
     extractor::guidance::TurnInstruction instruction;
     WaypointType waypoint_type;
     unsigned exit;
-    unsigned intersection;
+    std::vector<IntermediateIntersection> intersections;
 };
 } // namespace guidance
 } // namespace engine
