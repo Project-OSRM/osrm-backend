51a4cf5e46422906c1b5ff2ad042543c5e608b48
Moritz Kobitzsch <moritz@mapbox.com>
2016-03-23 10:41:28 +0100

    syncronize geometry and steps after post-processing

--- include/engine/guidance/post_processing.hpp
+++ include/engine/guidance/post_processing.hpp
@@ -2,6 +2,7 @@
 #define ENGINE_GUIDANCE_POST_PROCESSING_HPP
 
 #include "engine/guidance/route_step.hpp"
+#include "engine/guidance/leg_geometry.hpp"
 
 #include <vector>
 
@@ -12,8 +13,15 @@ namespace engine
 namespace guidance
 {
 
-//passed as none-reference to modify in-place and move out again
+// passed as none-reference to modify in-place and move out again
 std::vector<RouteStep> postProcess(std::vector<RouteStep> steps);
+
+// postProcess will break the connection between the leg geometry
+// for which a segment is supposed to represent exactly the coordinates
+// between routing maneuvers and the route steps itself.
+// If required, we can get both in sync again using this function.
+// Move in LegGeometry for modification in place.
+LegGeometry resyncGeometry(LegGeometry leg_geometry, const std::vector<RouteStep> &steps);
 
 } // namespace guidance
 } // namespace engine
