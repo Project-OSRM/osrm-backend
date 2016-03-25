0d291cb68ec5cb800b71856382963e55c8e7a98a
Lauren Budorick <lauren@mapbox.com>
2016-03-24 17:31:18 -0700

    Make cucumber executable via package.json bin field

--- cucumber.js
+++ cucumber.js
@@ -1,5 +1,5 @@
 module.exports = {
-    default: '--require features --tags ~@todo --tags ~@bug --tag ~@stress',
+    default: '--require features --tags ~@todo --tags ~@bug --tags ~@stress',
     verify: '--require features --tags ~@todo --tags ~@bug --tags ~@stress -f progress',
     jenkins: '--require features --tags ~@todo --tags ~@bug --tags ~@stress --tags ~@options -f progress',
     bugs: '--require features --tags @bug',
