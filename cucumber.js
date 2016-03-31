eeefb24a8e2ef1fcf6695abd628e2b1d30e9a643
Lauren Budorick <lauren@mapbox.com>
2016-03-31 12:33:48 -0700

    Remove one more compass column; update polyline encoding tests

--- cucumber.js
+++ cucumber.js
@@ -1,5 +1,5 @@
 module.exports = {
-    default: '--require features --tags ~@todo --tags ~@bug --tags ~@stress --tags ~@mokob',
+    default: '--require features --tags ~@todo --tags ~@bug --tags ~@stress',
     verify: '--require features --tags ~@todo --tags ~@bug --tags ~@stress -f progress',
     jenkins: '--require features --tags ~@todo --tags ~@bug --tags ~@stress --tags ~@options -f progress',
     bugs: '--require features --tags @bug',
