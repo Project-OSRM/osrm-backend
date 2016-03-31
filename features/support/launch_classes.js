94d3340d63d0a23bc6cd0f6ea014d558bf172026
Lauren Budorick <lauren@mapbox.com>
2016-03-30 18:20:37 -0700

    Fix double shutdown

--- features/support/launch_classes.js
+++ features/support/launch_classes.js
@@ -38,7 +38,7 @@ var OSRMBaseLoader = class {
     }
 
     osrmDown (callback) {
-        if (this.scope.pid) {
+        if (this.scope.pid && this.child && !this.child.exitCode) {
             process.kill(this.scope.pid, this.scope.TERMSIGNAL);
             this.waitForShutdown(callback);
             this.scope.pid = null;
