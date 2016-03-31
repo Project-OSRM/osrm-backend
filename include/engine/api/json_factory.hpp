475ae0b4a65e9de7a94c2d75aa5a4e445dcde415
Daniel Patterson <daniel@mapbox.com>
2016-03-30 18:36:56 -0700

    Move node information into route steps, and include nodes preceeding and tailing phantom nodes.

--- include/engine/api/json_factory.hpp
+++ include/engine/api/json_factory.hpp
@@ -60,12 +60,12 @@ util::json::Object makeGeoJSONLineString(ForwardIter begin, ForwardIter end)
 util::json::Object makeStepManeuver(const guidance::StepManeuver &maneuver);
 
 util::json::Object makeRouteStep(guidance::RouteStep step,
-                                 boost::optional<util::json::Value> geometry);
+                                 util::json::Value geometry,
+                                 util::json::Value nodes);
 
 util::json::Object makeRoute(const guidance::Route &route,
                              util::json::Array legs,
-                             boost::optional<util::json::Value> geometry,
-                             util::json::Array nodes);
+                             boost::optional<util::json::Value> geometry);
 
 util::json::Object
 makeWaypoint(const util::Coordinate location, std::string name, const Hint &hint);
@@ -73,7 +73,8 @@ makeWaypoint(const util::Coordinate location, std::string name, const Hint &hint
 util::json::Object makeRouteLeg(guidance::RouteLeg leg, util::json::Array steps);
 
 util::json::Array makeRouteLegs(std::vector<guidance::RouteLeg> legs,
-                                std::vector<util::json::Value> step_geometries);
+                                std::vector<util::json::Value> step_geometries,
+                                std::vector<util::json::Value> step_nodelists);
 }
 }
 } // namespace engine
