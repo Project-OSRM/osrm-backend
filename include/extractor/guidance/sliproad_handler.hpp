36d06815c8c96d6c678e70b50fb35715b9ba0852
Moritz Kobitzsch <moritz@mapbox.com>
2016-07-04 12:19:49 +0200

    improve sliproad / fork handling

--- include/extractor/guidance/sliproad_handler.hpp
+++ include/extractor/guidance/sliproad_handler.hpp
@@ -1,13 +1,16 @@
-#ifndef OSRM_EXTRACTOR_GUIDANCE_MOTORWAY_HANDLER_HPP_
-#define OSRM_EXTRACTOR_GUIDANCE_MOTORWAY_HANDLER_HPP_
+#ifndef OSRM_EXTRACTOR_GUIDANCE_SLIPROAD_HANDLER_HPP_
+#define OSRM_EXTRACTOR_GUIDANCE_SLIPROAD_HANDLER_HPP_
 
 #include "extractor/guidance/intersection.hpp"
 #include "extractor/guidance/intersection_handler.hpp"
+#include "extractor/guidance/intersection_generator.hpp"
 #include "extractor/query_node.hpp"
 
 #include "util/name_table.hpp"
 #include "util/node_based_graph.hpp"
 
+#include <cstddef>
+#include <utility>
 #include <vector>
 
 namespace osrm
@@ -19,37 +22,32 @@ namespace guidance
 
 // Intersection handlers deal with all issues related to intersections.
 // They assign appropriate turn operations to the TurnOperations.
-class MotorwayHandler : public IntersectionHandler
+class SliproadHandler : public IntersectionHandler
 {
   public:
-    MotorwayHandler(const util::NodeBasedDynamicGraph &node_based_graph,
+    SliproadHandler(const IntersectionGenerator &intersection_generator,
+                    const util::NodeBasedDynamicGraph &node_based_graph,
                     const std::vector<QueryNode> &node_info_list,
                     const util::NameTable &name_table,
                     const SuffixTable &street_name_suffix_table);
 
-    ~MotorwayHandler() override final = default;
+    ~SliproadHandler() override final = default;
 
     // check whether the handler can actually handle the intersection
-    bool canProcess(const NodeID nid,
-                    const EdgeID via_eid,
-                    const Intersection &intersection) const override final;
+    bool canProcess(const NodeID /*nid*/,
+                    const EdgeID /*via_eid*/,
+                    const Intersection & /*intersection*/) const override final;
 
     // process the intersection
     Intersection operator()(const NodeID nid,
                             const EdgeID via_eid,
                             Intersection intersection) const override final;
-
   private:
-    Intersection handleSliproads(const NodeID intersection_node_id,
-                                 Intersection intersection) const;
-    Intersection fromMotorway(const EdgeID via_edge, Intersection intersection) const;
-    Intersection fromRamp(const EdgeID via_edge, Intersection intersection) const;
-
-    Intersection fallback(Intersection intersection) const;
+    const IntersectionGenerator &intersection_generator;
 };
 
 } // namespace guidance
 } // namespace extractor
 } // namespace osrm
 
-#endif /*OSRM_EXTRACTOR_GUIDANCE_MOTORWAY_HANDLER_HPP_*/
+#endif /*OSRM_EXTRACTOR_GUIDANCE_SLIPROAD_HANDLER_HPP_*/
