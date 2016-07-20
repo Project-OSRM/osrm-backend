57e3f173d347ba0639308c920991d5ec00240684
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-07-20 15:58:48 +0200

    [skip ci] Update docker files to gcc 5

--- docker/run-gcc.sh
+++ docker/run-gcc.sh
@@ -5,7 +5,8 @@ set -o pipefail
 
 docker run \
     -i \
-    -e "CXX=g++" \
+    -e "CXX=g++-5" \
+    -e "CC=gcc-5" \
     -v `pwd`:/home/mapbox/osrm-backend \
     -t mapbox/osrm:linux \
     /bin/bash -lc "osrm-backend/docker/test.sh"
