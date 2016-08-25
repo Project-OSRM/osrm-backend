828b34d3ac1062db4ed87da928466c2e2a6974f8
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-08-25 16:52:31 +0200

    Swtich logging to writeStream

--- features/support/log.js
+++ features/support/log.js
@@ -6,26 +6,43 @@ const mkdirp = require('mkdirp');
 const d3 = require('d3-queue');
 
 module.exports = function () {
-    this.setupLogs = () => {
-        this.logQueue = d3.queue(1);
-    };
-
     this.setupScenarioLogFile = (featureID, scenarioID, callback) => {
+        this.logQueue = d3.queue(1);
+
         let logPath = path.join(this.LOGS_PATH, featureID);
-        this.scenarioLogFile = path.join(logPath, scenarioID) + '.log';
+        let scenarioLogFile = path.join(logPath, scenarioID) + '.log';
+
+        function openLogFile(file, callback) {
+          this.logStream = fs.createWriteStream(file);
+          this.logStream.on('open', () => { callback(); })
+          this.logStream.on('error', (err) => { throw err; })
+        }
 
 
         d3.queue(1)
           .defer(mkdirp, logPath)
-          .defer(fs.writeFile, this.scenarioLogFile, '')
+          .defer(openLogFile.bind(this), scenarioLogFile)
           .awaitAll(callback);
     };
 
-    this.finishLogs = (callback) => {
-        this.logQueue.awaitAll(callback);
+    this.finishScenarioLogs = (callback) => {
+        function finishStream(err) {
+          if (err) return callback(err);
+
+          this.logStream.on('finish', callback);
+          this.logStream.end();
+          this.logStream = null;
+        }
+        this.logQueue.awaitAll(finishStream.bind(this));
     };
 
     this.log = (message) => {
-        this.logQueue.defer(fs.appendFile, this.scenarioLogFile, message);
+        function writeToStream(message, callback) {
+          let wrote = this.logStream.write(message);
+          if (wrote) return callback();
+
+          this.logStream.once('drain', writeToStream.bind(this, message, callback));
+        }
+        this.logQueue.defer(writeToStream.bind(this), message);
     };
 };
