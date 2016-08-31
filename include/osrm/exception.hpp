8b144f22c97cdcc44044d7d9be25daa7374415a7
Daniel J. Hofmann <daniel@trvx.org>
2016-08-31 10:19:25 +0200

    Exposes osrm::exception for client usage, resolves #2813

--- include/osrm/exception.hpp
+++ include/osrm/exception.hpp
@@ -25,33 +25,14 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-#ifndef OSRM_EXCEPTION_HPP
-#define OSRM_EXCEPTION_HPP
+#ifndef GLOBAL_EXCEPTION_HPP
+#define GLOBAL_EXCEPTION_HPP
 
-#include <exception>
-#include <string>
-#include <utility>
+#include "util/exception.hpp"
 
 namespace osrm
 {
-namespace util
-{
-
-class exception final : public std::exception
-{
-  public:
-    explicit exception(const char *message) : message(message) {}
-    explicit exception(std::string message) : message(std::move(message)) {}
-    const char *what() const noexcept override { return message.c_str(); }
-
-  private:
-    // This function exists to 'anchor' the class, and stop the compiler from
-    // copying vtable and RTTI info into every object file that includes
-    // this header. (Caught by -Wweak-vtables under Clang.)
-    virtual void anchor() const;
-    const std::string message;
-};
-}
+using util::exception;
 }
 
-#endif /* OSRM_EXCEPTION_HPP */
+#endif
