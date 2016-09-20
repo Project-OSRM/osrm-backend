e36ab18d983231a7d4468c8780c7d970c246b57a
Dane Springmeyer <dane@mapbox.com>
2016-09-20 12:44:01 -0700

    stop using concurrency to install mason deps [skip ci]

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,32 +11,24 @@ function dep() {
 }
 
 function all_deps() {
-    FAIL=0
-    dep lua 5.3.0 &
-    dep luabind e414c57bcb687bb3091b7c55bbff6947f052e46b &
-    dep boost 1.61.0 &
-    dep boost_libatomic 1.61.0 &
-    dep boost_libchrono 1.61.0 &
-    dep boost_libsystem 1.61.0 &
-    dep boost_libthread 1.61.0 &
-    dep boost_libfilesystem 1.61.0 &
-    dep boost_libprogram_options 1.61.0 &
-    dep boost_libregex 1.61.0 &
-    dep boost_libiostreams 1.61.0 &
-    dep boost_libtest 1.61.0 &
-    dep boost_libdate_time 1.61.0 &
-    dep expat 2.1.1 &
-    dep stxxl 1.4.1 &
-    dep bzip2 1.0.6 &
-    dep zlib system &
-    dep tbb 43_20150316 &
-    for job in $(jobs -p)
-    do
-        wait $job || let "FAIL+=1"
-    done
-    if [[ "$FAIL" != "0" ]]; then
-        exit ${FAIL}
-    fi
+    dep lua 5.3.0
+    dep luabind e414c57bcb687bb3091b7c55bbff6947f052e46b
+    dep boost 1.61.0
+    dep boost_libatomic 1.61.0
+    dep boost_libchrono 1.61.0
+    dep boost_libsystem 1.61.0
+    dep boost_libthread 1.61.0
+    dep boost_libfilesystem 1.61.0
+    dep boost_libprogram_options 1.61.0
+    dep boost_libregex 1.61.0
+    dep boost_libiostreams 1.61.0
+    dep boost_libtest 1.61.0
+    dep boost_libdate_time 1.61.0
+    dep expat 2.1.1
+    dep stxxl 1.4.1
+    dep bzip2 1.0.6
+    dep zlib system
+    dep tbb 43_20150316
 }
 
 function main() {
