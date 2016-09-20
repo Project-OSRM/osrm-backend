463da4adb5e615eb590609ffaa16fbf22ff13ca9
kerrick-lyft <kstaley@lyft.com>
2016-09-20 10:26:53 -0700

    Smarter search radius formula for map matching

--- src/engine/plugins/match.cpp
+++ src/engine/plugins/match.cpp
@@ -153,15 +153,11 @@ Status MatchPlugin::HandleRequest(const std::shared_ptr<datafacade::BaseDataFaca
                        parameters.radiuses.end(),
                        search_radiuses.begin(),
                        [](const boost::optional<double> &maybe_radius) {
-                           if (maybe_radius)
-                           {
-                               return *maybe_radius * RADIUS_MULTIPLIER;
-                           }
-                           else
-                           {
-                               return DEFAULT_GPS_PRECISION * RADIUS_MULTIPLIER;
-                           }
+                           double in_radius = maybe_radius ? *maybe_radius : DEFAULT_GPS_PRECISION;
 
+                           return std::min(
+                               in_radius * parameters.search_radius_multiplier + parameters.search_radius_base,
+                               parameters.search_radius_max);
                        });
     }
 
