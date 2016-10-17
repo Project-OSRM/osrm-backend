424fd4def7bdd16da3a55ddedc089dc10b528f21
Kajari <kajari@mapbox.com>
2016-10-17 17:54:39 -0400

    added code for direction:ref:forward/backward, tests pass

--- profiles/lib/destination.lua
+++ profiles/lib/destination.lua
@@ -3,6 +3,8 @@ local Destination = {}
 function Destination.get_destination(way, direction)
   local destination = way:get_value_by_key("destination")
   local destination_ref = way:get_value_by_key("destination:ref")
+  local destination_ref_forward = way:get_value_by_key("destination:ref:forward")
+  local destination_ref_backward = way:get_value_by_key("destination:ref:backward")
   local destination_forward = way:get_value_by_key("destination:forward")
   local destination_backward = way:get_value_by_key("destination:backward")
 
@@ -11,8 +13,10 @@ function Destination.get_destination(way, direction)
 
   local rv = ""
 
-  if destination_ref and destination_ref ~= "" then
-    rv = rv .. string.gsub(destination_ref, ";", ", ")
+  if direction == "forward" and destination_ref == "" then
+    destination_ref = destination_ref_forward
+  elseif direction == "reverse" then
+    destination_ref = destination_ref_backward
   end
 
   if direction == "forward" and destination == "" then
@@ -21,6 +25,9 @@ function Destination.get_destination(way, direction)
     destination = destination_backward
   end
 
+  if destination_ref and destination_ref ~= "" then
+    rv = rv .. string.gsub(destination_ref, ";", ", ")
+  end
 
   if destination and destination ~= "" then
       if rv ~= "" then
