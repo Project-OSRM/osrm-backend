7925f813d3b4316f6a9fba87ebdd44882e54e474
Kajari <kajari@mapbox.com>
2016-10-18 14:56:41 -0400

    null pointer checks

--- profiles/lib/destination.lua
+++ profiles/lib/destination.lua
@@ -2,34 +2,40 @@ local Destination = {}
 
 function Destination.get_destination(way, is_forward)
   local destination = way:get_value_by_key("destination")
+  local destination_forward = way:get_value_by_key("destination:forward")
+  local destination_backward = way:get_value_by_key("destination:backward")
   local destination_ref = way:get_value_by_key("destination:ref")
   local destination_ref_forward = way:get_value_by_key("destination:ref:forward")
   local destination_ref_backward = way:get_value_by_key("destination:ref:backward")
-  local destination_forward = way:get_value_by_key("destination:forward")
-  local destination_backward = way:get_value_by_key("destination:backward")
 
   -- Assemble destination as: "A59: Düsseldorf, Köln"
   --          destination:ref  ^    ^  destination
-
+  
   local rv = ""
 
-  if is_forward == true and destination_ref == "" then
-    destination_ref = destination_ref_forward
+  if is_forward == true and destination_ref and destination_ref == "" then
+    if destination_ref_forward then
+      destination_ref = destination_ref_forward
+    end
   elseif is_forward == false then
-    destination_ref = destination_ref_backward
+    if destination_ref_backward then
+      destination_ref = destination_ref_backward
+    end
   end
 
-  if is_forward == true and destination == "" then
-    destination = destination_forward
+  if is_forward == true and destination and destination == "" then
+    if destination_forward then
+      destination = destination_forward
+    end
   elseif is_forward == false then
-    destination = destination_backward
+    if destination_backward then
+      destination = destination_backward
+    end
   end
 
-  if destination_ref and destination_ref ~= "" then
-    rv = rv .. string.gsub(destination_ref, ";", ", ")
-  end
-
-  if destination and destination ~= "" then
+  rv = rv .. string.gsub(destination_ref, ";", ", ")
+  
+  if destination ~= "" then
     if rv ~= "" then
         rv = rv .. ": "
     end
