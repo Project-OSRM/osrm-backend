47f1a7275835fd0cd9b38c9fbadeec46f8280262
Denis Koronchik <denis.koronchik@gmail.com>
2016-10-27 13:26:23 +0300

    Add support of custom ServiceHandler

--- include/server/request_handler.hpp
+++ include/server/request_handler.hpp
@@ -24,12 +24,12 @@ class RequestHandler
     RequestHandler(const RequestHandler &) = delete;
     RequestHandler &operator=(const RequestHandler &) = delete;
 
-    void RegisterServiceHandler(std::unique_ptr<ServiceHandler> service_handler);
+    void RegisterServiceHandler(std::unique_ptr<ServiceHandlerInterface> service_handler);
 
     void HandleRequest(const http::request &current_request, http::reply &current_reply);
 
   private:
-    std::unique_ptr<ServiceHandler> service_handler;
+    std::unique_ptr<ServiceHandlerInterface> service_handler;
 };
 }
 }
