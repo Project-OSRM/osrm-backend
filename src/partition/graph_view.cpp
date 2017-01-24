ec3f807206898ca186a39ddd51de06bb7a3ecc71
Moritz Kobitzsch <moritz@mapbox.com>
2017-01-24 16:25:29 +0100

    fix self-assignment/ thx daniel-j-h

--- src/partition/graph_view.cpp
+++ src/partition/graph_view.cpp
@@ -9,7 +9,7 @@ namespace partition
 HasSamePartitionID::HasSamePartitionID(const RecursiveBisectionState::BisectionID bisection_id_,
                                        const BisectionGraph &bisection_graph_,
                                        const RecursiveBisectionState &recursive_bisection_state_)
-    : bisection_id(bisection_id), bisection_graph(bisection_graph_),
+    : bisection_id(bisection_id_), bisection_graph(bisection_graph_),
       recursive_bisection_state(recursive_bisection_state_)
 {
 }
