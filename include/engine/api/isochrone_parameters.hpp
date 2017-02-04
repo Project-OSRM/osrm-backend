5018ef2487963d31ccdecdda09f35d2f38d9e51c
Daniel Patterson <daniel@mapbox.com>
2017-02-03 23:38:09 -0800

    Set default range to 5 minutes.

--- include/engine/api/isochrone_parameters.hpp
+++ include/engine/api/isochrone_parameters.hpp
@@ -50,7 +50,7 @@ namespace api
  */
 struct IsochroneParameters : public BaseParameters
 {
-    unsigned range = 10;
+    unsigned range = 300;
 
     bool IsValid() const { return BaseParameters::IsValid() && range >= 1; }
 };
