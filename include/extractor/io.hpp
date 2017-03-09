260a84b94bcc2881ddb6a05646fc7c55ee08d492
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2017-03-09 22:57:47 +0000

    Simplify write/read code

--- include/extractor/io.hpp
+++ include/extractor/io.hpp
@@ -28,11 +28,11 @@ template <> void read(const boost::filesystem::path &path, SegmentDataContainer 
     segment_data.fwd_durations.resize(num_entries);
     segment_data.rev_durations.resize(num_entries);
 
-    reader.ReadInto(segment_data.nodes.data(), segment_data.nodes.size());
-    reader.ReadInto(segment_data.fwd_weights.data(), segment_data.fwd_weights.size());
-    reader.ReadInto(segment_data.rev_weights.data(), segment_data.rev_weights.size());
-    reader.ReadInto(segment_data.fwd_durations.data(), segment_data.fwd_durations.size());
-    reader.ReadInto(segment_data.rev_durations.data(), segment_data.rev_durations.size());
+    reader.ReadInto(segment_data.nodes);
+    reader.ReadInto(segment_data.fwd_weights);
+    reader.ReadInto(segment_data.rev_weights);
+    reader.ReadInto(segment_data.fwd_durations);
+    reader.ReadInto(segment_data.rev_durations);
 }
 
 template <>
@@ -48,11 +48,11 @@ void write(const boost::filesystem::path &path, const SegmentDataContainer &segm
     writer.WriteElementCount32(segment_data.nodes.size());
 
     writer.WriteElementCount32(segment_data.nodes.size());
-    writer.WriteFrom(segment_data.nodes.data(), segment_data.nodes.size());
-    writer.WriteFrom(segment_data.fwd_weights.data(), segment_data.fwd_weights.size());
-    writer.WriteFrom(segment_data.rev_weights.data(), segment_data.rev_weights.size());
-    writer.WriteFrom(segment_data.fwd_durations.data(), segment_data.fwd_durations.size());
-    writer.WriteFrom(segment_data.rev_durations.data(), segment_data.rev_durations.size());
+    writer.WriteFrom(segment_data.nodes);
+    writer.WriteFrom(segment_data.fwd_weights);
+    writer.WriteFrom(segment_data.rev_weights);
+    writer.WriteFrom(segment_data.fwd_durations);
+    writer.WriteFrom(segment_data.rev_durations);
 }
 }
 }
