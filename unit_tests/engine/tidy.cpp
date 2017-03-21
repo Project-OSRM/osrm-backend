253fbd97492fa2074d80cdacce5de1fbb2fc8ff4
Lev Dragunov <lev@gojuno.com>
2017-03-21 12:44:21 +0300

    Test fixes

--- unit_tests/engine/tidy.cpp
+++ unit_tests/engine/tidy.cpp
@@ -29,12 +29,12 @@ BOOST_AUTO_TEST_CASE(two_item_trace_already_tidied_test)
     auto result = tidy::tidy(params, thresholds);
 
     BOOST_CHECK_EQUAL(result.can_be_removed.size(), 2);
-    BOOST_CHECK_EQUAL(result.original_to_tidied.size(), 2);
+    BOOST_CHECK_EQUAL(result.tidied_to_original.size(), 2);
 
     BOOST_CHECK(result.can_be_removed[0] == false);
     BOOST_CHECK(result.can_be_removed[1] == false);
-    BOOST_CHECK_EQUAL(result.original_to_tidied[0], 0);
-    BOOST_CHECK_EQUAL(result.original_to_tidied[1], 1);
+    BOOST_CHECK_EQUAL(result.tidied_to_original[0], 0);
+    BOOST_CHECK_EQUAL(result.tidied_to_original[1], 1);
 }
 
 BOOST_AUTO_TEST_CASE(two_item_trace_needs_tidiying_test)
@@ -53,12 +53,11 @@ BOOST_AUTO_TEST_CASE(two_item_trace_needs_tidiying_test)
     auto result = tidy::tidy(params, thresholds);
 
     BOOST_CHECK_EQUAL(result.can_be_removed.size(), 2);
-    BOOST_CHECK_EQUAL(result.original_to_tidied.size(), 2);
+    BOOST_CHECK_EQUAL(result.tidied_to_original.size(), 1);
 
     BOOST_CHECK_EQUAL(result.can_be_removed[0], false);
     BOOST_CHECK_EQUAL(result.can_be_removed[1], true);
-    BOOST_CHECK_EQUAL(result.original_to_tidied[0], 0);
-    BOOST_CHECK_EQUAL(result.original_to_tidied[1], 0);
+    BOOST_CHECK_EQUAL(result.tidied_to_original[0], 0);
 }
 
 BOOST_AUTO_TEST_CASE(two_blobs_in_traces_needs_tidiying_test)
@@ -88,27 +87,16 @@ BOOST_AUTO_TEST_CASE(two_blobs_in_traces_needs_tidiying_test)
     auto result = tidy::tidy(params, thresholds);
 
     BOOST_CHECK_EQUAL(result.can_be_removed.size(), params.coordinates.size());
-    BOOST_CHECK_EQUAL(result.original_to_tidied.size(), params.coordinates.size());
+    BOOST_CHECK_EQUAL(result.tidied_to_original.size(), 2);
 
-    auto valid = [](auto index) { return index == 0 || index == 3; };
-    auto ok = std::all_of(begin(result.original_to_tidied), end(result.original_to_tidied), valid);
-    BOOST_CHECK(ok);
-
-    BOOST_CHECK(std::is_sorted(begin(result.original_to_tidied), end(result.original_to_tidied)));
+    BOOST_CHECK_EQUAL(result.tidied_to_original[0], 0);
+    BOOST_CHECK_EQUAL(result.tidied_to_original[1], 3);
 
     const auto redundant = result.can_be_removed.count();
     BOOST_CHECK_EQUAL(redundant, params.coordinates.size() - 2);
 
     BOOST_CHECK_EQUAL(result.can_be_removed[0], false);
     BOOST_CHECK_EQUAL(result.can_be_removed[3], false);
-
-    auto second_blob_start = std::partition_point(begin(result.original_to_tidied),
-                                                  end(result.original_to_tidied),
-                                                  [](auto index) { return index == 0; });
-
-    BOOST_CHECK(begin(result.original_to_tidied) < second_blob_start);
-    BOOST_CHECK(second_blob_start < end(result.original_to_tidied));
-    BOOST_CHECK(second_blob_start == begin(result.original_to_tidied) + 3); // 3 in first blob
 }
 
 BOOST_AUTO_TEST_CASE(two_blobs_in_traces_needs_tidiying_no_timestamps_test)
@@ -130,27 +118,15 @@ BOOST_AUTO_TEST_CASE(two_blobs_in_traces_needs_tidiying_no_timestamps_test)
     auto result = tidy::tidy(params, thresholds);
 
     BOOST_CHECK_EQUAL(result.can_be_removed.size(), params.coordinates.size());
-    BOOST_CHECK_EQUAL(result.original_to_tidied.size(), params.coordinates.size());
-
-    auto valid = [](auto index) { return index == 0 || index == 3; };
-    auto ok = std::all_of(begin(result.original_to_tidied), end(result.original_to_tidied), valid);
-    BOOST_CHECK(ok);
-
-    BOOST_CHECK(std::is_sorted(begin(result.original_to_tidied), end(result.original_to_tidied)));
+    BOOST_CHECK_EQUAL(result.tidied_to_original.size(), 2);
+    BOOST_CHECK_EQUAL(result.tidied_to_original[0], 0);
+    BOOST_CHECK_EQUAL(result.tidied_to_original[1], 3);
 
     const auto redundant = result.can_be_removed.count();
     BOOST_CHECK_EQUAL(redundant, params.coordinates.size() - 2);
 
     BOOST_CHECK_EQUAL(result.can_be_removed[0], false);
     BOOST_CHECK_EQUAL(result.can_be_removed[3], false);
-
-    auto second_blob_start = std::partition_point(begin(result.original_to_tidied),
-                                                  end(result.original_to_tidied),
-                                                  [](auto index) { return index == 0; });
-
-    BOOST_CHECK(begin(result.original_to_tidied) < second_blob_start);
-    BOOST_CHECK(second_blob_start < end(result.original_to_tidied));
-    BOOST_CHECK(second_blob_start == begin(result.original_to_tidied) + 3); // 3 in first blob
 }
 
 BOOST_AUTO_TEST_SUITE_END()
