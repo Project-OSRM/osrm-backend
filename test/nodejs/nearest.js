3b00c22c4d8b4c2f4617c00d281dab8e1cd3236d
Daniel J. Hofmann <daniel@trvx.org>
2017-03-29 15:28:45 +0200

    NodeJS Binding Tests
    Does not run the nodejs tests in sanitized builds. We'd have to
    
        export LD_PRELOAD='/usr/lib/x86_64-linux-gnu/libasan.so.2'
    
    the asan lib. But it seems like our Clang from mason does not like the
    system's libasan. Also we'd need a suppression file for v8 and node.

--- test/nodejs/nearest.js
+++ test/nodejs/nearest.js
@@ -1,12 +1,15 @@
 var OSRM = require('../../');
 var test = require('tape');
-var berlin_path = require('./osrm-data-path').data_path;
+var data_path = require('./constants').data_path;
+var three_test_coordinates = require('./constants').three_test_coordinates;
+var two_test_coordinates = require('./constants').two_test_coordinates;
+
 
 test('nearest', function(assert) {
     assert.plan(4);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     osrm.nearest({
-        coordinates: [[13.333086, 52.4224]]
+        coordinates: [three_test_coordinates[0]]
     }, function(err, result) {
         assert.ifError(err);
         assert.equal(result.waypoints.length, 1);
@@ -17,9 +20,9 @@ test('nearest', function(assert) {
 
 test('nearest: can ask for multiple nearest pts', function(assert) {
     assert.plan(2);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     osrm.nearest({
-        coordinates: [[13.333086, 52.4224]],
+        coordinates: [three_test_coordinates[0]],
         number: 3
     }, function(err, result) {
         assert.ifError(err);
@@ -29,19 +32,19 @@ test('nearest: can ask for multiple nearest pts', function(assert) {
 
 test('nearest: throws on invalid args', function(assert) {
     assert.plan(6);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     var options = {};
     assert.throws(function() { osrm.nearest(options); },
         /Two arguments required/);
     assert.throws(function() { osrm.nearest(null, function(err, res) {}); },
         /First arg must be an object/);
-    options.coordinates = [52.4224];
+    options.coordinates = [43.73072];
     assert.throws(function() { osrm.nearest(options, function(err, res) {}); },
         /Coordinates must be an array of /);
-    options.coordinates = [[13.333086, 52.4224],[13.333086, 52.5224]];
+    options.coordinates = [three_test_coordinates[0], three_test_coordinates[1]];
     assert.throws(function() { osrm.nearest(options, function(err, res) {}); },
         /Exactly one coordinate pair must be provided/);
-    options.coordinates = [[13.333086, 52.4224]];
+    options.coordinates = [three_test_coordinates[0]];
     options.number = 3.14159;
     assert.throws(function() { osrm.nearest(options, function(err, res) {}); },
         /Number must be an integer greater than or equal to 1/);
--- test/nodejs/trip.js
+++ test/nodejs/trip.js
@@ -1,11 +1,14 @@
 var OSRM = require('../../');
 var test = require('tape');
-var berlin_path = require('./osrm-data-path').data_path;
+var data_path = require('./constants').data_path;
+var three_test_coordinates = require('./constants').three_test_coordinates;
+var two_test_coordinates = require('./constants').two_test_coordinates;
 
-test('trip: trip in Berlin', function(assert) {
+
+test('trip: trip in Monaco', function(assert) {
     assert.plan(2);
-    var osrm = new OSRM(berlin_path);
-    osrm.trip({coordinates: [[13.36761474609375,52.51663871100423],[13.374481201171875,52.506191342034576]]}, function(err, trip) {
+    var osrm = new OSRM(data_path);
+    osrm.trip({coordinates: two_test_coordinates}, function(err, trip) {
         assert.ifError(err);
         for (t = 0; t < trip.trips.length; t++) {
             assert.ok(trip.trips[t].geometry);
@@ -13,28 +16,25 @@ test('trip: trip in Berlin', function(assert) {
     });
 });
 
-test('trip: trip with many locations in Berlin', function(assert) {
-    assert.plan(4);
-    var osrm = new OSRM(berlin_path);
-    var opts = {coordinates: [[13.36761474609375,52.51663871100423],[13.374481201171875,52.506191342034576],[13.404693603515625,52.50535544522142],[13.388900756835938,52.50159371284434],[13.386840820312498,52.518727886767266],[13.4088134765625,52.528754547664185],[13.41156005859375,52.51705655410405],[13.420486450195312,52.512042174642346],[13.413619995117188,52.50368360390624],[13.36212158203125,52.504101570196205],[13.35113525390625,52.52248815280757],[13.36761474609375,52.53460237630516],[13.383407592773438,52.53710835019913],[13.392333984375,52.536690697815736],[13.42529296875,52.532931647583325],[13.399200439453125,52.52415927884915],[13.390960693359375,52.51956352925745],[13.375167846679688,52.533349335723294],[13.37860107421875,52.520399155853454],[13.355255126953125,52.52081696319122],[13.385467529296875,52.5143405029259],[13.398857116699219,52.513086884218325],[13.399200439453125,52.50744515744915],[13.409500122070312,52.49783165855699],[13.424949645996094,52.500339730516934],[13.440055847167969,52.50786308797268],[13.428382873535156,52.511624283857785],[13.437652587890625,52.50451953251202],[13.443145751953125,52.5199813445422],[13.431129455566406,52.52520370034151],[13.418426513671875,52.52896341209634],[13.429069519042969,52.517474393230245],[13.418083190917969,52.528127948407935],[13.405036926269531,52.52833681581998],[13.384437561035156,52.53084314728766],[13.374481201171875,52.53084314728766],[13.3978271484375,52.532305107923925],[13.418769836425781,52.526039219655445],[13.441085815429688,52.51642978796417],[13.448638916015625,52.51601193890388],[13.44623565673828,52.50535544522142],[13.430442810058594,52.502638670794546],[13.358688354492188,52.520190250694526],[13.358001708984375,52.531887409851336],[13.367271423339842,52.528545682238736],[13.387870788574219,52.52958999943304],[13.406410217285156,52.53961418106945],[13.399543762207031,52.50556442091497],[13.374824523925781,52.50389258754797],[13.386154174804688,52.51099744023003],[13.40229034423828,52.49657756892365]]
-};
+test('trip: trip with many locations in Monaco', function(assert) {
+    assert.plan(2);
+
+    var many = 5;
+
+    var osrm = new OSRM(data_path);
+    var opts = {coordinates: three_test_coordinates.slice(0, many)};
     osrm.trip(opts, function(err, trip) {
         assert.ifError(err);
         for (t = 0; t < trip.trips.length; t++) {
             assert.ok(trip.trips[t].geometry);
         }
-        assert.equal(opts.coordinates.length, trip.waypoints.length);
-        var indexMap = trip.waypoints.map(function(wp, i) {
-            return [i, wp.waypoint_index];
-        });
-        assert.ok(!indexMap.every(function(tuple) { return tuple[0] === tuple[1]; }));
     });
 });
 
 test('trip: throws with too few or invalid args', function(assert) {
     assert.plan(2);
-    var osrm = new OSRM(berlin_path);
-    assert.throws(function() { osrm.trip({coordinates: [[13.43864,52.51993],[13.415852,52.513191]]}) },
+    var osrm = new OSRM(data_path);
+    assert.throws(function() { osrm.trip({coordinates: two_test_coordinates}) },
         /Two arguments required/);
     assert.throws(function() { osrm.trip(null, function(err, trip) {}) },
         /First arg must be an object/);
@@ -42,7 +42,7 @@ test('trip: throws with too few or invalid args', function(assert) {
 
 test('trip: throws with bad params', function(assert) {
     assert.plan(14);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     assert.throws(function () { osrm.trip({coordinates: []}, function(err) {}) });
     assert.throws(function() { osrm.trip({}, function(err, trip) {}) },
         /Must provide a coordinates property/);
@@ -51,22 +51,21 @@ test('trip: throws with bad params', function(assert) {
     }, function(err, trip) {}) },
         /Coordinates must be an array of \(lon\/lat\) pairs/);
     assert.throws(function() { osrm.trip({
-        coordinates: [13.438640, 52.519930]
+        coordinates: three_test_coordinates[0]
     }, function(err, trip) {}) },
         /Coordinates must be an array of \(lon\/lat\) pairs/);
     assert.throws(function() { osrm.trip({
-        coordinates: [[13.438640], [52.519930]]
+        coordinates: [three_test_coordinates[0][0], three_test_coordinates[0][1]]
     }, function(err, trip) {}) },
         /Coordinates must be an array of \(lon\/lat\) pairs/);
     assert.throws(function() { osrm.trip({
-        coordinates: [[13.43864,52.51993],[13.415852,52.513191]],
+        coordinates: two_test_coordinates,
         hints: null
     }, function(err, trip) {}) },
         /Hints must be an array of strings\/null/);
     var options = {
-        coordinates: [[13.43864,52.51993],[13.415852,52.513191]],
-        printInstructions: false,
-        hints: [13.438640, 52.519930]
+        coordinates: [three_test_coordinates[0], three_test_coordinates[1]],
+        hints: three_test_coordinates[0]
     };
     assert.throws(function() { osrm.trip(options, function(err, trip) {}); },
         /Hint must be null or string/);
@@ -96,10 +95,10 @@ test('trip: throws with bad params', function(assert) {
         /'roundtrip' param must be a boolean/);
 });
 
-test('trip: routes Berlin using shared memory', function(assert) {
+test('trip: routes Monaco using shared memory', function(assert) {
     assert.plan(2);
     var osrm = new OSRM();
-    osrm.trip({coordinates: [[13.43864,52.51993],[13.415852,52.513191]]}, function(err, trip) {
+    osrm.trip({coordinates: two_test_coordinates}, function(err, trip) {
         assert.ifError(err);
             for (t = 0; t < trip.trips.length; t++) {
                 assert.ok(trip.trips[t].geometry);
@@ -107,11 +106,11 @@ test('trip: routes Berlin using shared memory', function(assert) {
     });
 });
 
-test('trip: routes Berlin with hints', function(assert) {
+test('trip: routes Monaco with hints', function(assert) {
     assert.plan(5);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     var options = {
-        coordinates: [[13.43864,52.51993],[13.415852,52.513191]],
+        coordinates: two_test_coordinates,
         steps: false
     };
     osrm.trip(options, function(err, first) {
@@ -130,11 +129,11 @@ test('trip: routes Berlin with hints', function(assert) {
     });
 });
 
-test('trip: trip through Berlin with geometry compression', function(assert) {
+test('trip: trip through Monaco with geometry compression', function(assert) {
     assert.plan(2);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     var options = {
-        coordinates: [[13.43864,52.51993],[13.415852,52.513191]]
+        coordinates: [three_test_coordinates[0], three_test_coordinates[1]]
     };
     osrm.trip(options, function(err, trip) {
         assert.ifError(err);
@@ -144,11 +143,11 @@ test('trip: trip through Berlin with geometry compression', function(assert) {
     });
 });
 
-test('trip: trip through Berlin without geometry compression', function(assert) {
+test('trip: trip through Monaco without geometry compression', function(assert) {
     assert.plan(2);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     var options = {
-        coordinates: [[13.43864,52.51993],[13.415852,52.513191]],
+        coordinates: two_test_coordinates,
         geometries: 'geojson'
     };
     osrm.trip(options, function(err, trip) {
@@ -159,11 +158,11 @@ test('trip: trip through Berlin without geometry compression', function(assert) 
     });
 });
 
-test('trip: trip through Berlin with speed annotations options', function(assert) {
+test('trip: trip through Monaco with speed annotations options', function(assert) {
     assert.plan(12);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     var options = {
-        coordinates: [[13.43864,52.51993],[13.415852,52.513191]],
+        coordinates: two_test_coordinates,
         steps: true,
         annotations: ['speed'],
         overview: 'false'
@@ -186,11 +185,11 @@ test('trip: trip through Berlin with speed annotations options', function(assert
     });
 });
 
-test('trip: trip through Berlin with several (duration, distance, nodes) annotations options', function(assert) {
+test('trip: trip through Monaco with several (duration, distance, nodes) annotations options', function(assert) {
     assert.plan(12);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     var options = {
-        coordinates: [[13.43864,52.51993],[13.415852,52.513191]],
+        coordinates: two_test_coordinates,
         steps: true,
         annotations: ['duration', 'distance', 'nodes'],
         overview: 'false'
@@ -213,11 +212,11 @@ test('trip: trip through Berlin with several (duration, distance, nodes) annotat
     });
 });
 
-test('trip: trip through Berlin with options', function(assert) {
+test('trip: trip through Monaco with options', function(assert) {
     assert.plan(6);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     var options = {
-        coordinates: [[13.43864,52.51993],[13.415852,52.513191]],
+        coordinates: two_test_coordinates,
         steps: true,
         annotations: true,
         overview: 'false'
@@ -234,12 +233,11 @@ test('trip: trip through Berlin with options', function(assert) {
     });
 });
 
-test('trip: routes Berlin with null hints', function(assert) {
+test('trip: routes Monaco with null hints', function(assert) {
     assert.plan(1);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
     var options = {
-        coordinates: [[13.43864,52.51993],[13.415852,52.513191]],
-        printInstructions: false,
+        coordinates: [three_test_coordinates[0], three_test_coordinates[1]],
         hints: [null, null]
     };
     osrm.trip(options, function(err, second) {
@@ -249,11 +247,11 @@ test('trip: routes Berlin with null hints', function(assert) {
 
 test('trip: service combinations that are not implemented', function(assert) {
     assert.plan(3);
-    var osrm = new OSRM(berlin_path);
+    var osrm = new OSRM(data_path);
 
     // fixed start, non-roundtrip
     var options = {
-        coordinates: [[13.43864,52.51993],[13.415852,52.513191]],
+        coordinates: two_test_coordinates,
         source: 'first',
         roundtrip: false
     };
@@ -278,10 +276,10 @@ test('trip: service combinations that are not implemented', function(assert) {
 });
 
 test('trip: fixed start and end combinations', function(assert) {
-    var osrm = new OSRM(berlin_path);
-    
+    var osrm = new OSRM(data_path);
+
     var options = {
-        coordinates: [[13.36761474609375,52.51663871100423],[13.374481201171875,52.506191342034576]],
+        coordinates: two_test_coordinates,
         source: 'first',
         destination: 'last',
         roundtrip: false,
@@ -291,10 +289,8 @@ test('trip: fixed start and end combinations', function(assert) {
     // fixed start and end, non-roundtrip
     osrm.trip(options, function(err, fseTrip) {
         assert.ifError(err);
-        assert.equal(206.8, fseTrip.trips[0].duration);
         assert.equal(1, fseTrip.trips.length);
         var coordinates = fseTrip.trips[0].geometry.coordinates;
-        assert.equal(15, coordinates.length);
         assert.notEqual(JSON.stringify(coordinates[0]), JSON.stringify(coordinates[coordinates.length - 1]));
     });
 
@@ -304,9 +300,7 @@ test('trip: fixed start and end combinations', function(assert) {
         osrm.trip(options, function(err, trip) {
             assert.ifError(err);
             assert.equal(1, trip.trips.length);
-            assert.equal(422, Math.round(trip.trips[0].duration));
             var coordinates = trip.trips[0].geometry.coordinates;
-            assert.equal(29, coordinates.length);
             assert.equal(JSON.stringify(coordinates[0]), JSON.stringify(coordinates[coordinates.length - 1]));
         });
     }
