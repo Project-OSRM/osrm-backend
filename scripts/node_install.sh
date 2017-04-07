05706879a0a1e9ab0542f472c1d74f675ab53a44
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2017-04-07 09:44:43 +0000

    Less is more: Don't force any specific build configuration

--- scripts/node_install.sh
+++ scripts/node_install.sh
@@ -2,27 +2,19 @@
 
 set -e
 
-CMAKE_OPTIONS=${CMAKE_OPTIONS:-"-DCMAKE_BUILD_TYPE=Release -DENABLE_NODE_BINDINGS=On -DENABLE_MASON=On"}
+CMAKE_OPTIONS=${CMAKE_OPTIONS:-"-DCMAKE_BUILD_TYPE=Release -DENABLE_NODE_BINDINGS=On"}
 
 if [[ ! -f $(which cmake) ]]; then
     echo "Needs cmake to build from source"
     exit 1
 fi
-if [[ ! -f $(which clang) ]]; then
-    echo "Needs clang to build from source"
-    exit 1
-fi
-if [[ ! -f $(which clang++) ]]; then
-    echo "Needs clang++ to build from source"
-    exit 1
-fi
-
 if [[ -d build ]]; then
     echo "Detected existing build directory, skipping compiling."
     exit 0
 else
     mkdir -p build
     pushd build
-    CXX=clang++ CC=clang cmake .. $CMAKE_OPTIONS
+    cmake .. $CMAKE_OPTIONS
+    cmake --build .
     popd
 fi
