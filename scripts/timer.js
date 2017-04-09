2c001b63a5eba35fcf9b1eb5e8675f41587aa2ab
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2017-04-09 11:52:05 +0000

    Port timer script to JS

---
+++ scripts/timer.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+var name = process.argv[2];
+var cmd = process.argv.slice(3).join(' ');
+var start = +new Date();
+exec(cmd, (err) => {
+    if (err) return console.log(err);
+    var stop = +new Date();
+    var time = (stop - start) / 1000.;
+    fs.appendFileSync('/tmp/osrm.timings', `${name}\t${time}`, 'utf-8')
+})
+
+
