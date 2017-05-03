b15a998b2519e5df1cf02a2aee73689d62afdcbc
Michael Krasnyk <michael.krasnyk@gmail.com>
2017-05-03 16:16:29 +0200

    Remove GetEdgeBasedNodeID from TurnDataContainer

--- include/extractor/turn_data_container.hpp
+++ include/extractor/turn_data_container.hpp
@@ -40,20 +40,17 @@ template <storage::Ownership Ownership> class TurnDataContainerImpl
   public:
     TurnDataContainerImpl() = default;
 
-    TurnDataContainerImpl(Vector<NodeID> node_data_ids,
-                          Vector<extractor::guidance::TurnInstruction> turn_instructions,
+    TurnDataContainerImpl(Vector<extractor::guidance::TurnInstruction> turn_instructions,
                           Vector<LaneDataID> lane_data_ids,
                           Vector<EntryClassID> entry_class_ids,
                           Vector<util::guidance::TurnBearing> pre_turn_bearings,
                           Vector<util::guidance::TurnBearing> post_turn_bearings)
-        : node_data_ids(std::move(node_data_ids)), turn_instructions(std::move(turn_instructions)),
-          lane_data_ids(std::move(lane_data_ids)), entry_class_ids(std::move(entry_class_ids)),
+        : turn_instructions(std::move(turn_instructions)), lane_data_ids(std::move(lane_data_ids)),
+          entry_class_ids(std::move(entry_class_ids)),
           pre_turn_bearings(std::move(pre_turn_bearings)),
           post_turn_bearings(std::move(post_turn_bearings))
     {
     }
-
-    NodeID GetNodeID(const EdgeID id) const { return node_data_ids[id]; }
 
     EntryClassID GetEntryClassID(const EdgeID id) const { return entry_class_ids[id]; }
 
@@ -78,14 +75,12 @@ template <storage::Ownership Ownership> class TurnDataContainerImpl
 
     // Used by EdgeBasedGraphFactory to fill data structure
     template <typename = std::enable_if<Ownership == storage::Ownership::Container>>
-    void push_back(NodeID node_data_id,
-                   extractor::guidance::TurnInstruction turn_instruction,
+    void push_back(extractor::guidance::TurnInstruction turn_instruction,
                    LaneDataID lane_data_id,
                    EntryClassID entry_class_id,
                    util::guidance::TurnBearing pre_turn_bearing,
                    util::guidance::TurnBearing post_turn_bearing)
     {
-        node_data_ids.push_back(node_data_id);
         turn_instructions.push_back(turn_instruction);
         lane_data_ids.push_back(lane_data_id);
         entry_class_ids.push_back(entry_class_id);
@@ -99,7 +94,6 @@ template <storage::Ownership Ownership> class TurnDataContainerImpl
                                                 const TurnDataContainerImpl &turn_data_container);
 
   private:
-    Vector<NodeID> node_data_ids;
     Vector<extractor::guidance::TurnInstruction> turn_instructions;
     Vector<LaneDataID> lane_data_ids;
     Vector<EntryClassID> entry_class_ids;
