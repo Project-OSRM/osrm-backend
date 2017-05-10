a3e7d8510828e63df080574f8eddddc58667e9f5
Michael Krasnyk <michael.krasnyk@gmail.com>
2017-05-10 21:42:53 +0200

    Added dummy TZ shapefile with test timezone polygons

--- cucumber.js
+++ cucumber.js
@@ -1,7 +1,9 @@
 module.exports = {
-    default: '--strict --tags ~@stress --tags ~@todo --require features/support --require features/step_definitions',
-    verify: '--strict --tags ~@stress --tags ~@todo -f progress --require features/support --require features/step_definitions',
+    default: '--strict --tags ~@stress --tags ~@todo --tags ~@shapelib --require features/support --require features/step_definitions',
+    verify: '--strict --tags ~@stress --tags ~@todo --tags ~@shapelib -f progress --require features/support --require features/step_definitions',
     todo: '--strict --tags @todo --require features/support --require features/step_definitions',
     all: '--strict --require features/support --require features/step_definitions',
-    mld: '--strict --tags ~@stress --tags ~@todo --tags ~@alternative --tags ~@matrix --tags ~@trip --require features/support --require features/step_definitions -f progress',
+    mld: '--strict --tags ~@stress --tags ~@todo --tags ~@alternative --tags ~@matrix --tags ~@trip --tags ~@shapelib --require features/support --require features/step_definitions -f progress',
+    conditionals: '--strict --tags @conditionals --require features/support --require features/step_definitions',
+    mld_conditionals: '--strict --tags @conditionals --require features/support --require features/step_definitions -f progress'
 }
