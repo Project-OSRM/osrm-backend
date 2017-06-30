2e9be5b9a09ed729cb48c7292ffa66f2ef27a425
Michael Krasnyk <michael.krasnyk@gmail.com>
2017-06-30 23:51:14 +0200

    Generalize directShortestPathSearch interface

--- include/engine/routing_algorithms/direct_shortest_path.hpp
+++ include/engine/routing_algorithms/direct_shortest_path.hpp
@@ -21,20 +21,11 @@ namespace routing_algorithms
 /// by the previous route.
 /// This variation is only an optimazation for graphs with slow queries, for example
 /// not fully contracted graphs.
-InternalRouteResult directShortestPathSearch(
-    SearchEngineData<ch::Algorithm> &engine_working_data,
-    const datafacade::ContiguousInternalMemoryDataFacade<ch::Algorithm> &facade,
-    const PhantomNodes &phantom_nodes);
-
-InternalRouteResult directShortestPathSearch(
-    SearchEngineData<corech::Algorithm> &engine_working_data,
-    const datafacade::ContiguousInternalMemoryDataFacade<corech::Algorithm> &facade,
-    const PhantomNodes &phantom_nodes);
-
-InternalRouteResult directShortestPathSearch(
-    SearchEngineData<mld::Algorithm> &engine_working_data,
-    const datafacade::ContiguousInternalMemoryDataFacade<mld::Algorithm> &facade,
-    const PhantomNodes &phantom_nodes);
+template <typename Algorithm>
+InternalRouteResult
+directShortestPathSearch(SearchEngineData<Algorithm> &engine_working_data,
+                         const datafacade::ContiguousInternalMemoryDataFacade<Algorithm> &facade,
+                         const PhantomNodes &phantom_nodes);
 
 } // namespace routing_algorithms
 } // namespace engine
