16f69a8619ad21172e3fb9f4dfcc63733de20c71
vng <viktor.govako@gmail.com>
2017-07-21 15:59:54 +0300

    Compilation fix.

--- include/extractor/edge_based_graph_factory.hpp
+++ include/extractor/edge_based_graph_factory.hpp
@@ -40,11 +40,6 @@
 #include <unordered_set>
 #include <vector>
 
-#include <boost/filesystem/fstream.hpp>
-
-#include <tbb/concurrent_unordered_map.h>
-#include <tbb/concurrent_vector.h>
-
 namespace osrm
 {
 namespace extractor
