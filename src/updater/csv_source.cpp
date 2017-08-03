4757c96d9a91cc7fb3ad61bcb2ef9a2a9283017d
Michael Krasnyk <michael.krasnyk@gmail.com>
2017-08-03 14:56:14 +0200

    Add a check for non-empty segments in CSV speed files

--- src/updater/csv_source.cpp
+++ src/updater/csv_source.cpp
@@ -36,7 +36,19 @@ SegmentLookupTable readSegmentValues(const std::vector<std::string> &paths)
     CSVFilesParser<Segment, SpeedSource> parser(
         1, qi::ulong_long >> ',' >> qi::ulong_long, qi::uint_ >> -(',' >> qi::double_));
 
-    return parser(paths);
+    // Check consistency of keys in the result lookup table
+    auto result = parser(paths);
+    const auto found_inconsistency =
+        std::find_if(std::begin(result.lookup), std::end(result.lookup), [](const auto &entry) {
+            return entry.first.from == entry.first.to;
+        });
+    if (found_inconsistency != std::end(result.lookup))
+    {
+        throw util::exception("empty segment in CSV with node " +
+                              std::to_string(found_inconsistency->first.from) + " " + SOURCE_REF);
+    }
+
+    return std::move(result);
 }
 
 TurnLookupTable readTurnValues(const std::vector<std::string> &paths)
