3f7b5da68345bbdc1fd28abe14e6508ba7e1776a
karenzshea <karenzshea@gmail.com>
2017-11-28 13:09:32 +0100

    check empty name string in turn collapsing

--- include/engine/api/route_api.hpp
+++ include/engine/api/route_api.hpp
@@ -173,7 +173,7 @@ class RouteAPI : public BaseAPI
 
                 guidance::trimShortSegments(steps, leg_geometry);
                 leg.steps = guidance::handleRoundabouts(std::move(steps));
-                leg.steps = guidance::collapseTurnInstructions(std::move(leg.steps));
+                leg.steps = guidance::collapseTurnInstructions(std::move(leg.steps), facade);
                 leg.steps = guidance::anticipateLaneChange(std::move(leg.steps));
                 leg.steps = guidance::buildIntersections(std::move(leg.steps));
                 leg.steps = guidance::suppressShortNameSegments(std::move(leg.steps));
