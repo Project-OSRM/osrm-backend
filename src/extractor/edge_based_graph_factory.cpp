5bd15f4557506c1d4cdbe28306f43baf1c773514
Michael Krasnyk <michael.krasnyk@gmail.com>
2018-01-02 10:54:46 +0100

    Use consistent EBG node weights in duplicated via nodes

--- src/extractor/edge_based_graph_factory.cpp
+++ src/extractor/edge_based_graph_factory.cpp
@@ -375,7 +375,9 @@ EdgeBasedGraphFactory::GenerateEdgeExpandedNodes(const WayRestrictionMap &way_re
             m_edge_based_node_container.nodes[edge_based_node_id].segregated =
                 segregated_edges.count(eid) > 0;
 
-            m_edge_based_node_weights.push_back(m_edge_based_node_weights[eid]);
+            const auto ebn_weight = m_edge_based_node_weights[nbe_to_ebn_mapping[eid]];
+            BOOST_ASSERT(ebn_weight == INVALID_EDGE_WEIGHT || ebn_weight == edge_data.weight);
+            m_edge_based_node_weights.push_back(ebn_weight);
 
             edge_based_node_id++;
             progress.PrintStatus(progress_counter++);
