3ae343c043b97fd2eddf43b50164174fb174e17e
Kajari Ghosh <ghoshkaj@gmail.com>
2018-03-30 19:32:49 -0400

    swapped out unordered_map cache with a boost_lru implementation

--- include/engine/unpacking_cache.hpp
+++ include/engine/unpacking_cache.hpp
@@ -1,72 +1,47 @@
 #ifndef UNPACKING_CACHE_HPP
 #define UNPACKING_CACHE_HPP
 
+#include <boost/optional/optional_io.hpp>
+
+#include "../../third_party/compute_detail/lru_cache.hpp"
 #include "util/typedefs.hpp"
 
-#include <boost/functional/hash_fwd.hpp>
-#include <unordered_map>
-#include <utility>
-
-namespace std
-{
-template <> struct hash<std::tuple<NodeID, NodeID, std::size_t>>
-{
-    typedef std::tuple<NodeID, NodeID, std::size_t> argument_type;
-    typedef std::size_t result_type;
-    result_type operator()(argument_type const &tuple) const noexcept
-    {
-        result_type seed = 0;
-        result_type const h1(std::hash<unsigned int>{}(std::get<0>(tuple)));
-        result_type const h2(std::hash<unsigned int>{}(std::get<1>(tuple)));
-        result_type const h3(std::hash<unsigned int>{}(std::get<2>(tuple)));
-
-        boost::hash_combine(seed, h1);
-        boost::hash_combine(seed, h2);
-        boost::hash_combine(seed, h3);
-
-        return seed;
-    }
-};
-}
 namespace osrm
 {
 namespace engine
 {
 class UnpackingCache
 {
-    std::unordered_map<std::tuple<NodeID, NodeID, std::size_t>, EdgeDuration> cache;
-
   private:
+    boost::compute::detail::lru_cache<std::tuple<NodeID, NodeID, std::size_t>, EdgeDuration> cache;
     unsigned current_data_timestamp = 0;
 
   public:
-    UnpackingCache(unsigned timestamp) : current_data_timestamp(timestamp){};
-    // UnpackingCache(){};
+    UnpackingCache(unsigned timestamp) : cache(6000000), current_data_timestamp(timestamp){};
 
     void Clear(unsigned new_data_timestamp)
     {
         if (current_data_timestamp != new_data_timestamp)
         {
             cache.clear();
+            current_data_timestamp = new_data_timestamp;
         }
     }
 
     bool IsEdgeInCache(std::tuple<NodeID, NodeID, std::size_t> edge)
     {
-        bool edge_is_in_cache = cache.find(edge) != cache.end();
-        return edge_is_in_cache;
+        return cache.contains(edge);
     }
 
     void AddEdge(std::tuple<NodeID, NodeID, std::size_t> edge, EdgeDuration duration)
     {
-        cache.insert({edge, duration});
-        GetDuration(edge);
+        cache.insert(edge, duration);
     }
 
     EdgeDuration GetDuration(std::tuple<NodeID, NodeID, std::size_t> edge)
     {
-        EdgeDuration duration = cache[edge];
-        return duration;
+        boost::optional<EdgeDuration> duration = cache.get(edge);
+        return *duration ? *duration : MAXIMAL_EDGE_DURATION;
     }
 };
 } // engine
