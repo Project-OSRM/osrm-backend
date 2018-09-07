6ca4627fff0bd203057635e85bf9bb30c1af82a3
Daniel Patterson <danpat@danpat.net>
2018-09-07 03:23:17 -0700

    Store edge distances to improve matrix distance calculation.

--- include/util/debug.hpp
+++ include/util/debug.hpp
@@ -1,9 +1,11 @@
 #ifndef OSRM_UTIL_DEBUG_HPP_
 #define OSRM_UTIL_DEBUG_HPP_
 
+#include "extractor/edge_based_edge.hpp"
 #include "extractor/node_data_container.hpp"
 #include "extractor/query_node.hpp"
 #include "guidance/intersection.hpp"
+#include "guidance/turn_instruction.hpp"
 #include "guidance/turn_lane_data.hpp"
 #include "engine/guidance/route_step.hpp"
 #include "util/node_based_graph.hpp"
@@ -25,7 +27,7 @@ inline std::ostream &operator<<(std::ostream &out, const Coordinate &coordinate)
         << toFloating(coordinate.lat) << "}";
     return out;
 }
-}
+} // namespace util
 
 namespace engine
 {
@@ -60,8 +62,8 @@ inline std::ostream &operator<<(std::ostream &out, const RouteStep &step)
 
     return out;
 }
-}
-}
+} // namespace guidance
+} // namespace engine
 
 namespace guidance
 {
@@ -74,7 +76,7 @@ inline std::ostream &operator<<(std::ostream &out, const ConnectedRoad &road)
         << static_cast<std::int32_t>(road.lane_data_id) << "}";
     return out;
 }
-}
+} // namespace guidance
 
 namespace extractor
 {
@@ -93,7 +95,7 @@ inline std::ostream &operator<<(std::ostream &out, const IntersectionViewData &v
         << " angle: " << view.angle << " bearing: " << view.perceived_bearing << "}";
     return out;
 }
-}
+} // namespace intersection
 
 namespace TurnLaneType
 {
@@ -123,9 +125,9 @@ inline std::ostream &operator<<(std::ostream &out, const Mask lane_type)
 
     return out;
 }
-}
-}
-}
+} // namespace TurnLaneType
+} // namespace extractor
+} // namespace osrm
 
 namespace std
 {
@@ -145,7 +147,7 @@ inline std::ostream &operator<<(std::ostream &out,
 
     return out;
 }
-}
+} // namespace std
 
 namespace osrm
 {
@@ -184,8 +186,26 @@ inline std::ostream &operator<<(std::ostream &out, const LaneDataVector &turn_la
 
     return out;
 }
+} // namespace lanes
+} // namespace guidance
+
+namespace extractor
+{
+inline std::ostream &operator<<(std::ostream &out, const EdgeBasedEdge &edge)
+{
+    out << " EdgeBasedEdge {";
+    out << " source " << edge.source << ", target: " << edge.target;
+    out << " EdgeBasedEdgeData data {";
+    out << " turn_id: " << edge.data.turn_id << ", weight: " << edge.data.weight;
+    out << " distance: " << edge.data.distance << ", duration: " << edge.data.duration;
+    out << " forward: " << (edge.data.forward == 0 ? "false" : "true")
+        << ", backward: " << (edge.data.backward == 0 ? "false" : "true");
+    out << " }";
+    out << "}";
+    return out;
 }
-}
-}
+
+} // namespace extractor
+} // namespace osrm
 
 #endif /*OSRM_ENGINE_GUIDANCE_DEBUG_HPP_*/
