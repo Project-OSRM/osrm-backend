0ab85a2478ed2651933dfe6c3ff9e2304ff0fd14
whizzrahul <56035717+whizzrahul@users.noreply.github.com>
2019-10-01 20:15:11 +0530

    Method name wrong

--- cucumber.js
+++ cucumber.js
@@ -1,4 +1,4 @@
-module.exports = {
+mode.exports = {
     default: '--strict --tags ~@stress --tags ~@todo --tags ~@mld-only --require features/support --require features/step_definitions',
     verify: '--strict --tags ~@stress --tags ~@todo --tags ~@mld-only -f progress --require features/support --require features/step_definitions',
     todo: '--strict --tags @todo --require features/support --require features/step_definitions',
