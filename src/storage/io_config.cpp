1e67c3b02c5746930a74aaf350c2cb4e40d13121
Rafael Guglielmetti <rafael.guglielmetti@bestmile.com>
2020-10-29 09:25:21 +0100

    Fix windows build

--- src/storage/io_config.cpp
+++ src/storage/io_config.cpp
@@ -32,7 +32,7 @@ std::vector<std::string> IOConfig::GetMissingFiles() const
     std::vector<std::string> missingFiles;
     for (auto &fileName : required_input_files)
     {
-        if (!boost::filesystem::is_regular_file({base_path.string() + fileName.string()}))
+        if (!boost::filesystem::is_regular_file(boost::filesystem::path(base_path.string() + fileName.string())))
         {
             missingFiles.push_back(base_path.string() + fileName.string());
         }
