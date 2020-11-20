e693b8963a31e64bd98b0fe0835cc57e281128be
Desone Burns II <dburnsii@live.com>
2020-11-19 17:15:17 -0700

    Remove unsigned 0 comparison

--- include/engine/datafacade_factory.hpp
+++ include/engine/datafacade_factory.hpp
@@ -70,7 +70,7 @@ template <template <typename A> class FacadeT, typename AlgorithmT> class DataFa
                              "The exclude prefix needs to be a valid data path.");
             std::size_t index =
                 std::stoi(exclude_prefix.substr(index_begin + 1, exclude_prefix.size()));
-            BOOST_ASSERT(index >= 0 && index < facades.size());
+            BOOST_ASSERT(index < facades.size());
             facades[index] = std::make_shared<const Facade>(allocator, metric_name, index);
         }
 
