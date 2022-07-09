8dc021476e53a53f61bf88a1d964f61fb88e73be
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2022-07-09 21:51:54 +0200

    Migrate to C++17. Update sol2 to 3.3.0.

--- unit_tests/partitioner/bisection_graph_view.cpp
+++ unit_tests/partitioner/bisection_graph_view.cpp
@@ -16,7 +16,8 @@ using namespace osrm::util;
 
 BOOST_AUTO_TEST_SUITE(graph_view)
 
-static void shuffle(std::vector<EdgeWithSomeAdditionalData>& grid_edges) {
+static void shuffle(std::vector<EdgeWithSomeAdditionalData>& grid_edges)
+{
     std::random_device rd;
     std::mt19937 rng(rd());
     std::shuffle(grid_edges.begin(), grid_edges.end(), rng);
