d1838961d1f27ad3b3a1833ff5a3cd9c0e062a4a
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2022-08-21 22:24:20 +0200

    Update io_config.cpp

--- src/storage/io_config.cpp
+++ src/storage/io_config.cpp
@@ -17,8 +17,7 @@ bool IOConfig::IsValid() const
     bool success = true;
     for (auto &fileName : required_input_files)
     {
-        if (!boost::filesystem::is_regular_file(
-                boost::filesystem::path(base_path.string() + fileName.string())))
+        if (!fs::is_regular_file(fs::path(base_path.string() + fileName.string())))
         {
             util::Log(logWARNING) << "Missing/Broken File: " << base_path.string()
                                   << fileName.string();
@@ -33,8 +32,7 @@ std::vector<std::string> IOConfig::GetMissingFiles() const
     std::vector<std::string> missingFiles;
     for (auto &fileName : required_input_files)
     {
-        if (!boost::filesystem::is_regular_file(
-                boost::filesystem::path(base_path.string() + fileName.string())))
+        if (!fs::is_regular_file(fs::path(base_path.string() + fileName.string())))
         {
             missingFiles.push_back(base_path.string() + fileName.string());
         }
