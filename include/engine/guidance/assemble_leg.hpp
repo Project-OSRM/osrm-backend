f1ad21180499aaab0f6485e6aec257dc3fd294fc
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2022-09-09 15:04:56 +0200

    wip

--- include/engine/guidance/assemble_leg.hpp
+++ include/engine/guidance/assemble_leg.hpp
@@ -6,8 +6,8 @@
 #include "engine/guidance/route_leg.hpp"
 #include "engine/guidance/route_step.hpp"
 #include "engine/internal_route_result.hpp"
-#include "util/typedefs.hpp"
 #include "util/coordinate_calculation.hpp"
+#include "util/typedefs.hpp"
 
 #include <boost/algorithm/string/join.hpp>
 #include <boost/range/adaptor/filtered.hpp>
