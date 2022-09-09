467fafb188da284b1020e76af4e74be3e94a750d
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2022-09-09 15:23:27 +0200

    wip

--- env.sh
+++
@@ -1,3 +0,0 @@
-set -e
-PACKAGE_JSON_VERSION=$(node -e "console.log(require('./package.json').version)")
-echo PUBLISH=$([[ "${GITHUB_REF:-}" == "refs/tags/v${PACKAGE_JSON_VERSION}" ]] && echo "On" || echo "Off") >> $GITHUB_ENV
