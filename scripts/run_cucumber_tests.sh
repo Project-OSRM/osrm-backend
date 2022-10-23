dd04760982ca04986d42d5a04137a77f81a20e4b
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2022-10-23 14:38:42 +0200

    Implement NodeJS based server fully replicating osrm-routed

--- scripts/run_cucumber_tests.sh
+++ scripts/run_cucumber_tests.sh
@@ -3,12 +3,12 @@ set -e
 
 export OSRM_USE_ROUTED_JS=1
 node ./node_modules/cucumber/bin/cucumber.js features/ -p verify_routed_js
-# node ./node_modules/cucumber/bin/cucumber.js features/ -p verify_routed_js -m mmap
-# node ./node_modules/cucumber/bin/cucumber.js features/ -p mld_routed_js 
-# node ./node_modules/cucumber/bin/cucumber.js features/ -p mld_routed_js -m mmap
+node ./node_modules/cucumber/bin/cucumber.js features/ -p verify_routed_js -m mmap
+node ./node_modules/cucumber/bin/cucumber.js features/ -p mld_routed_js 
+node ./node_modules/cucumber/bin/cucumber.js features/ -p mld_routed_js -m mmap
 unset OSRM_USE_ROUTED_JS
 
-# node ./node_modules/cucumber/bin/cucumber.js features/ -p verify
-# node ./node_modules/cucumber/bin/cucumber.js features/ -p verify -m mmap
-# node ./node_modules/cucumber/bin/cucumber.js features/ -p mld 
-# node ./node_modules/cucumber/bin/cucumber.js features/ -p mld -m mmap
+node ./node_modules/cucumber/bin/cucumber.js features/ -p verify
+node ./node_modules/cucumber/bin/cucumber.js features/ -p verify -m mmap
+node ./node_modules/cucumber/bin/cucumber.js features/ -p mld 
+node ./node_modules/cucumber/bin/cucumber.js features/ -p mld -m mmap
