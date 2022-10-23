6dfb7d4eded25322313ecff6bc770dc0ad234096
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2022-10-23 16:57:01 +0200

    Implement NodeJS based server fully replicating osrm-routed

--- scripts/run_cucumber_tests.sh
+++ scripts/run_cucumber_tests.sh
@@ -4,11 +4,16 @@ set -e
 # we do not run `osrm-routed-js` tests on v12 since fastify doesn't support it
 NODE_VERSION=$(node --version)
 NODE_MAJOR_VERSION=$(echo $NODE_VERSION | cut -d. -f1)
-if [[ $NODE_MAJOR_VERSION != "v12" ]]; then
+if [[ "$NODE_MAJOR_VERSION" != "v12" ]]; then
+    echo "Running osrm-routed-js tests"
+    
     export OSRM_USE_ROUTED_JS=1
     node ./node_modules/cucumber/bin/cucumber.js features/ -p verify_routed_js
     node ./node_modules/cucumber/bin/cucumber.js features/ -p mld_routed_js
     unset OSRM_USE_ROUTED_JS
+else 
+    echo "Skipping osrm-routed-js tests on Node.js ${NODE_VERSION}"
+fi
 fi
 
 node ./node_modules/cucumber/bin/cucumber.js features/ -p verify
