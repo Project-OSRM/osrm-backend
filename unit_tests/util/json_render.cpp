16c5a64bbdb19408e1989224ce92d3bbce2fa6eb
张峰 <zhangfeng.z@bytedance.com>
2023-01-30 17:11:27 +0800

    fix(json_render): add json render unittest & optimized Number formatter.

--- unit_tests/util/json_render.cpp
+++ unit_tests/util/json_render.cpp
@@ -16,7 +16,7 @@ BOOST_AUTO_TEST_CASE(number_truncating)
 
     // this number would have more than 10 decimals if not truncated
     renderer(Number{42.9995999594999399299});
-    BOOST_CHECK_EQUAL(str, "42.999599959");
+    BOOST_CHECK_EQUAL(str, "42.99959996");
 }
 
 BOOST_AUTO_TEST_CASE(integer)
@@ -25,6 +25,13 @@ BOOST_AUTO_TEST_CASE(integer)
     Renderer<std::string> renderer(str);
     renderer(Number{42.0});
     BOOST_CHECK_EQUAL(str, "42");
+}
+
+BOOST_AUTO_TEST_CASE(test_json_issue_6531) {
+    std::string output;
+    osrm::util::json::Renderer<std::string> renderer(output);
+    renderer(0.0000000000017114087924596788);
+    BOOST_CHECK_EQUAL(output, "1.711408792e-12");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
