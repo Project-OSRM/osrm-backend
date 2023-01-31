a32dfed070fcd3c8e8c426145c42d6f81829fa2c
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2023-01-31 14:56:57 +0100

    Fix format

--- unit_tests/util/json_render.cpp
+++ unit_tests/util/json_render.cpp
@@ -27,7 +27,8 @@ BOOST_AUTO_TEST_CASE(integer)
     BOOST_CHECK_EQUAL(str, "42");
 }
 
-BOOST_AUTO_TEST_CASE(test_json_issue_6531) {
+BOOST_AUTO_TEST_CASE(test_json_issue_6531)
+{
     std::string output;
     osrm::util::json::Renderer<std::string> renderer(output);
     renderer(0.0000000000017114087924596788);
