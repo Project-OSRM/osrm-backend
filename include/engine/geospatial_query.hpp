36c074ca6069a5c5e4bad44ad3ce1addd8be6123
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2024-05-12 11:57:19 +0200

    Optimise R-tree queries in the case if there is no need to limit maximum number of results

--- include/engine/geospatial_query.hpp
+++ include/engine/geospatial_query.hpp
@@ -88,13 +88,11 @@ template <typename RTreeT, typename DataFacadeT> class GeospatialQuery
                 return valid;
             });
         auto phantom_nodes = MakePhantomNodes(input_coordinate, results);
-        // there is no guarantee that `SearchInRange` will return results sorted by distance, 
+        // there is no guarantee that `SearchInRange` will return results sorted by distance,
         // but we may rely on it somewhere
-        std::sort(phantom_nodes.begin(), phantom_nodes.end(), [](const auto &lhs,
-                                                                        const auto &rhs)
-            {
-                return lhs.distance < rhs.distance;
-            });
+        std::sort(phantom_nodes.begin(),
+                  phantom_nodes.end(),
+                  [](const auto &lhs, const auto &rhs) { return lhs.distance < rhs.distance; });
         return phantom_nodes;
 #endif
     }
