cf8d479782cbb60e4bfc37114aeb86c8ff36a3de
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2024-05-26 21:28:49 +0200

    wip

--- scripts/ci/run_benchmarks.sh
+++ scripts/ci/run_benchmarks.sh
@@ -12,15 +12,14 @@ function run_benchmarks_for_folder {
     BENCHMARKS_FOLDER="$FOLDER/build/src/benchmarks"
 
     ./$BENCHMARKS_FOLDER/match-bench "./$FOLDER/test/data/mld/monaco.osrm" mld > "$RESULTS_FOLDER/match_mld.bench"
-    # ./$BENCHMARKS_FOLDER/match-bench "./$FOLDER/test/data/ch/monaco.osrm" ch > "$RESULTS_FOLDER/match_ch.bench"
-    # ./$BENCHMARKS_FOLDER/route-bench "./$FOLDER/test/data/mld/monaco.osrm" mld > "$RESULTS_FOLDER/route_mld.bench"
-    # ./$BENCHMARKS_FOLDER/route-bench "./$FOLDER/test/data/ch/monaco.osrm" ch > "$RESULTS_FOLDER/route_ch.bench"
-    # ./$BENCHMARKS_FOLDER/alias-bench > "$RESULTS_FOLDER/alias.bench"
-    # ./$BENCHMARKS_FOLDER/json-render-bench  "./$FOLDER/src/benchmarks/portugal_to_korea.json" > "$RESULTS_FOLDER/json-render.bench"
-    # ./$BENCHMARKS_FOLDER/packedvector-bench > "$RESULTS_FOLDER/packedvector.bench"
-    # ./$BENCHMARKS_FOLDER/rtree-bench "./$FOLDER/test/data/monaco.osrm.ramIndex" "./$FOLDER/test/data/monaco.osrm.fileIndex" "./$FOLDER/test/data/monaco.osrm.nbg_nodes" > "$RESULTS_FOLDER/rtree.bench"
+    ./$BENCHMARKS_FOLDER/match-bench "./$FOLDER/test/data/ch/monaco.osrm" ch > "$RESULTS_FOLDER/match_ch.bench"
+    ./$BENCHMARKS_FOLDER/route-bench "./$FOLDER/test/data/mld/monaco.osrm" mld > "$RESULTS_FOLDER/route_mld.bench"
+    ./$BENCHMARKS_FOLDER/route-bench "./$FOLDER/test/data/ch/monaco.osrm" ch > "$RESULTS_FOLDER/route_ch.bench"
+    ./$BENCHMARKS_FOLDER/alias-bench > "$RESULTS_FOLDER/alias.bench"
+    ./$BENCHMARKS_FOLDER/json-render-bench  "./$FOLDER/src/benchmarks/portugal_to_korea.json" > "$RESULTS_FOLDER/json-render.bench"
+    ./$BENCHMARKS_FOLDER/packedvector-bench > "$RESULTS_FOLDER/packedvector.bench"
+    ./$BENCHMARKS_FOLDER/rtree-bench "./$FOLDER/test/data/monaco.osrm.ramIndex" "./$FOLDER/test/data/monaco.osrm.fileIndex" "./$FOLDER/test/data/monaco.osrm.nbg_nodes" > "$RESULTS_FOLDER/rtree.bench"
 
-    # TODO: CH
     BINARIES_FOLDER="$FOLDER/build"
     echo "PWD: $FOLDER"
     cp ~/data.osm.pbf $FOLDER
@@ -29,28 +28,30 @@ function run_benchmarks_for_folder {
     $BINARIES_FOLDER/osrm-customize $FOLDER/data.osrm
     $BINARIES_FOLDER/osrm-contract $FOLDER/data.osrm
 
-    # TODO: save results
     if [ -f "$FOLDER/scripts/ci/locustfile.py" ]; then
-        $BINARIES_FOLDER/osrm-routed --algorithm mld $FOLDER/data.osrm &
-        OSRM_ROUTED_PID=$!
+        for ALGORITHM in mld ch; do
+            $BINARIES_FOLDER/osrm-routed --algorithm $ALGORITHM $FOLDER/data.osrm &
+            OSRM_ROUTED_PID=$!
 
-        # wait for osrm-routed to start
-        curl --retry-delay 3 --retry 10 --retry-all-errors "http://127.0.0.1:5000/route/v1/driving/13.388860,52.517037;13.385983,52.496891?steps=true"
-        locust -f $FOLDER/scripts/ci/locustfile.py \
-            --headless \
-            --processes -1 \
-            --users 10 \
-            --spawn-rate 1 \
-            --host http://localhost:5000 \
-            --run-time 1m \
-            --csv=locust_results \
-            --loglevel ERROR
+            # wait for osrm-routed to start
+            curl --retry-delay 3 --retry 10 --retry-all-errors "http://127.0.0.1:5000/route/v1/driving/13.388860,52.517037;13.385983,52.496891?steps=true"
+            locust -f $FOLDER/scripts/ci/locustfile.py \
+                --headless \
+                --processes -1 \
+                --users 10 \
+                --spawn-rate 1 \
+                --host http://localhost:5000 \
+                --run-time 1m \
+                --csv=locust_results_$ALGORITHM \
+                --loglevel ERROR || true
 
-        python3 $FOLDER/scripts/ci/process_locust_benchmark_results.py locust_results mld $RESULTS_FOLDER
+            python3 $FOLDER/scripts/ci/process_locust_benchmark_results.py locust_results_$ALGORITHM $ALGORITHM $RESULTS_FOLDER
 
 
-        kill -0 $OSRM_ROUTED_PID
+            kill -0 $OSRM_ROUTED_PID
+        done
     fi
+
 }
 
 run_benchmarks_for_folder $1 "${1}_results"
