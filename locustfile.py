fe672dd373207a6a949408cc30f35b7cf431084e
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2024-05-26 13:25:43 +0200

    wip

--- locustfile.py
+++ locustfile.py
@@ -8,6 +8,11 @@ class OSRMTasks(TaskSet):
         
         self.client.get(f"/route/v1/driving/{start};{end}?overview=full&steps=true")
 
+    @task
+    def get_table(self):
+        coordinates = "13.388860,52.517037;13.397634,52.529407;13.428555,52.523219"
+        self.client.get(f"/table/v1/driving/{coordinates}")
+
 class OSRMUser(HttpUser):
     tasks = [OSRMTasks]
     wait_time = between(1, 5)
