0f5ffc2a84370e67e1b1cbd6650346112eb5c12e
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2024-05-30 19:19:30 +0200

    Try to use ankerl::unordered_dense::map instead of std::unordered_map in UnorderedMapStorage

--- include/util/query_heap.hpp
+++ include/util/query_heap.hpp
@@ -9,53 +9,11 @@
 #include <limits>
 #include <map>
 #include <optional>
-#include <unordered_map>
+#include <ankerl/unordered_dense.h>
 #include <vector>
 
 namespace osrm::util
 {
-
-template <typename NodeID, typename Key> class GenerationArrayStorage
-{
-    using GenerationCounter = std::uint16_t;
-
-  public:
-    explicit GenerationArrayStorage(std::size_t size)
-        : positions(size, 0), generation(1), generations(size, 0)
-    {
-    }
-
-    Key &operator[](NodeID node)
-    {
-        generation[node] = generation;
-        return positions[node];
-    }
-
-    Key peek_index(const NodeID node) const
-    {
-        if (generations[node] < generation)
-        {
-            return std::numeric_limits<Key>::max();
-        }
-        return positions[node];
-    }
-
-    void Clear()
-    {
-        generation++;
-        // if generation overflows we end up at 0 again and need to clear the vector
-        if (generation == 0)
-        {
-            generation = 1;
-            std::fill(generations.begin(), generations.end(), 0);
-        }
-    }
-
-  private:
-    GenerationCounter generation;
-    std::vector<GenerationCounter> generations;
-    std::vector<Key> positions;
-};
 
 template <typename NodeID, typename Key> class ArrayStorage
 {
@@ -98,7 +56,9 @@ template <typename NodeID, typename Key> class MapStorage
 template <typename NodeID, typename Key> class UnorderedMapStorage
 {
   public:
-    explicit UnorderedMapStorage(std::size_t) { nodes.rehash(1000); }
+    explicit UnorderedMapStorage(std::size_t) { 
+        nodes.rehash(1000); 
+        }
 
     Key &operator[](const NodeID node) { return nodes[node]; }
 
@@ -121,7 +81,7 @@ template <typename NodeID, typename Key> class UnorderedMapStorage
     void Clear() { nodes.clear(); }
 
   private:
-    std::unordered_map<NodeID, Key> nodes;
+   ankerl::unordered_dense::map<NodeID, Key> nodes;
 };
 
 template <typename NodeID,
