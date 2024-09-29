5359a7774e332726c7e7331cc17fe1610627ae23
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2024-09-29 10:11:37 +0200

    Migrate to Conan 2.x

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+
+class OsrmConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    requires = (
+        "boost/1.85.0",
+        "bzip2/1.0.8",
+        "expat/2.6.2",
+        "lua/5.4.6",
+        "onetbb/2021.12.0",
+    )
+    generators = "CMakeDeps"
+    
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_CXX_STANDARD"] = "20"
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
