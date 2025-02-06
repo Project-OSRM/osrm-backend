9384ccab82bd80c33823e46e060178b1a22740c3
Stepan Kizim <stsiapan.kizim@gmail.com>
2025-02-06 21:12:51 +0100

    fix: Remove accidental change for boost 1.87

--- include/server/server.hpp
+++ include/server/server.hpp
@@ -52,7 +52,8 @@ class Server
         const auto port_string = std::to_string(port);
 
         boost::asio::ip::tcp::resolver resolver(io_context);
-        boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(address, port_string).begin();
+        boost::asio::ip::tcp::resolver::query query(address, port_string);
+        boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(query);
 
         acceptor.open(endpoint.protocol());
 #ifdef SO_REUSEPORT
