f206198950f71fc3ce988c41b63d72ba2a882d50
Alexander Farber <farber72@outlook.de>
2025-08-16 19:48:09 +0200

    Add new OSRMWorld class, convert hooks.js

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { BeforeAll, Before, After, AfterAll } = require('@cucumber/cucumber');
 var d3 = require('d3-queue');
 var path = require('path');
 var fs = require('fs');
@@ -7,60 +8,55 @@ var OSM = require('../lib/osm');
 var OSRMLoader = require('../lib/osrm_loader');
 const { createDir } = require('../lib/utils');
 
-module.exports = function () {
-  this.registerHandler('BeforeFeatures', { timeout: 30000 }, (features, callback) => {
-    this.osrmLoader = new OSRMLoader(this);
-    this.OSMDB = new OSM.DB();
+BeforeAll({ timeout: 30000 }, function (callback) {
+  this.osrmLoader = new OSRMLoader(this);
+  this.OSMDB = new OSM.DB();
 
-    let queue = d3.queue(1);
-    queue.defer(this.initializeEnv.bind(this));
-    queue.defer(this.verifyOSRMIsNotRunning.bind(this));
-    queue.defer(this.verifyExistenceOfBinaries.bind(this));
-    queue.defer(this.initializeCache.bind(this));
-    queue.defer(this.setupFeatures.bind(this, features));
-    queue.awaitAll(callback);
-  });
+  let queue = d3.queue(1);
+  queue.defer(this.initializeEnv.bind(this));
+  queue.defer(this.verifyOSRMIsNotRunning.bind(this));
+  queue.defer(this.verifyExistenceOfBinaries.bind(this));
+  queue.defer(this.initializeCache.bind(this));
+  queue.defer(this.setupFeatures.bind(this, [])); // features parameter not available in BeforeAll
+  queue.awaitAll(callback);
+});
 
-  this.BeforeFeature((feature, callback) => {
-    this.profile = this.OSRM_PROFILE || this.DEFAULT_PROFILE;
-    this.profileFile = path.join(this.PROFILES_PATH, this.profile + '.lua');
-    this.setupFeatureCache(feature);
-    callback();
-  });
+Before(function (scenario, callback) {
+  this.profile = this.OSRM_PROFILE || this.DEFAULT_PROFILE;
+  this.profileFile = path.join(this.PROFILES_PATH, this.profile + '.lua');
+  this.setupFeatureCache(scenario.gherkinDocument);
 
-  this.Before((scenario, callback) => {
-    this.osrmLoader.setLoadMethod(this.DEFAULT_LOAD_METHOD);
-    this.setGridSize(this.DEFAULT_GRID_SIZE);
-    this.setOrigin(this.DEFAULT_ORIGIN);
-    this.queryParams = {};
-    this.extractArgs = '';
-    this.contractArgs = '';
-    this.partitionArgs = '';
-    this.customizeArgs = '';
-    this.loaderArgs = '';
-    this.environment = Object.assign(this.DEFAULT_ENVIRONMENT);
-    this.resetOSM();
+  this.osrmLoader.setLoadMethod(this.DEFAULT_LOAD_METHOD);
+  this.setGridSize(this.DEFAULT_GRID_SIZE);
+  this.setOrigin(this.DEFAULT_ORIGIN);
+  this.queryParams = {};
+  this.extractArgs = '';
+  this.contractArgs = '';
+  this.partitionArgs = '';
+  this.customizeArgs = '';
+  this.loaderArgs = '';
+  this.environment = Object.assign(this.DEFAULT_ENVIRONMENT);
+  this.resetOSM();
 
-    this.scenarioID = this.getScenarioID(scenario);
-    this.setupScenarioCache(this.scenarioID);
+  this.scenarioID = this.getScenarioID(scenario);
+  this.setupScenarioCache(this.scenarioID);
 
-    // setup output logging
-    let logDir = path.join(this.LOGS_PATH, this.featureID);
-    this.scenarioLogFile = path.join(logDir, this.scenarioID) + '.log';
-    d3.queue(1)
-      .defer(createDir, logDir)
-      .defer((callback) => fs.rm(this.scenarioLogFile, { force: true }, callback))
-      .awaitAll(callback);
-    // uncomment to get path to logfile
-    // console.log('  Writing logging output to ' + this.scenarioLogFile);
-  });
+  // setup output logging
+  let logDir = path.join(this.LOGS_PATH, this.featureID);
+  this.scenarioLogFile = path.join(logDir, this.scenarioID) + '.log';
+  d3.queue(1)
+    .defer(createDir, logDir)
+    .defer((callback) => fs.rm(this.scenarioLogFile, { force: true }, callback))
+    .awaitAll(callback);
+  // uncomment to get path to logfile
+  // console.log('  Writing logging output to ' + this.scenarioLogFile);
+});
 
-  this.After((scenario, callback) => {
-    this.resetOptionsOutput();
-    callback();
-  });
+After(function (scenario, callback) {
+  this.resetOptionsOutput();
+  callback();
+});
 
-  this.AfterFeatures((features, callback) => {
-    callback();
-  });
-};
+AfterAll(function (callback) {
+  callback();
+});
