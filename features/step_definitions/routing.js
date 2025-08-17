67f09e2631422c1f93faa95ebe662217d0556e53
Alexander Farber <farber72@outlook.de>
2025-08-17 17:44:14 +0200

    Use the new Given call, with regexes. Use functions instead of arrow syntax to have the access to this object

--- features/step_definitions/routing.js
+++ features/step_definitions/routing.js
@@ -1,23 +1,24 @@
 // Step definitions for testing basic routing API functionality
 var d3 = require('d3-queue');
+const { When, Given } = require('@cucumber/cucumber');
 
-module.exports = function () {
-  this.When(/^I route I should get$/, this.WhenIRouteIShouldGet);
+When(/^I route I should get$/, function (table, callback) {
+  this.WhenIRouteIShouldGet(table, callback);
+});
 
-  // Runs routing test multiple times for performance testing
-  this.When(/^I route (\d+) times I should get$/, { timeout: 100 * this.TIMEOUT }, (n, table, callback) => {
-    var q = d3.queue(1);
+// Runs routing test multiple times for performance testing
+When(/^I route (\d+) times I should get$/, { timeout: 100 * this.TIMEOUT }, function (n, table, callback) {
+  var q = d3.queue(1);
 
-    for (var i=0; i<n; i++) {
-      q.defer(this.WhenIRouteIShouldGet, table);
-    }
+  for (var i=0; i<n; i++) {
+    q.defer(this.WhenIRouteIShouldGet, table);
+  }
 
-    q.awaitAll(callback);
-  });
+  q.awaitAll(callback);
+});
 
-  // Sets skip_waypoints parameter for routing requests
-  this.Given(/^skip waypoints$/, (callback) => {
-    this.queryParams['skip_waypoints'] = true;
-    callback();
-  });
-};
+// Sets skip_waypoints parameter for routing requests
+Given(/^skip waypoints$/, function (callback) {
+  this.queryParams['skip_waypoints'] = true;
+  callback();
+});
