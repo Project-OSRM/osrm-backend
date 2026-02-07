cdba01ab16c3f1b2a648543941e744bc4bc1a0e2
Alexander Farber <farber72@outlook.de>
2026-02-07 18:22:12 +0100

    Fix formatting

--- src/tools/routed.cpp
+++ src/tools/routed.cpp
@@ -119,9 +119,9 @@ inline unsigned generateServerProgramOptions(const int argc,
 
     // declare a group of options that will be allowed only on command line
     boost::program_options::options_description generic_options("Options");
-    generic_options.add_options()            //
-        ("version,v", "Show version")        //
-        ("help,h", "Show this help message") //
+    generic_options.add_options()                                           //
+        ("version,v", "Show version")                                       //
+        ("help,h", "Show this help message")                                //
         ("list-inputs", "List required and optional input file extensions") //
         ("verbosity,l",
 #ifdef NDEBUG
