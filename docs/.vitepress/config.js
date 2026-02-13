d738b6e8e9aca483ce4db35676d288e9275ba40d
Dennis Luxen <ich@dennisluxen.de>
2026-02-13 15:37:23 +0100

    Migrate API docs generation to vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,77 @@
+import { defineConfig } from 'vitepress'
+import { endpointPlugin } from './plugins/endpoint.js'
+
+export default defineConfig({
+  title: 'OSRM API Documentation',
+  description: 'The Open Source Routing Machine API documentation',
+  base: '/osrm-backend/',
+  ignoreDeadLinks: true,
+
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'HTTP API', link: '/http' },
+      { text: 'Node.js API', link: '/nodejs/api' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Getting Started',
+        items: [
+          { text: 'Introduction', link: '/' },
+          { text: 'Developing', link: '/developing' },
+          { text: 'Testing', link: '/testing' },
+          { text: 'Releasing', link: '/releasing' }
+        ]
+      },
+      {
+        text: 'API Documentation',
+        items: [
+          { text: 'HTTP API', link: '/http' },
+          { text: 'Node.js API', link: '/nodejs/api' }
+        ]
+      },
+      {
+        text: 'Configuration',
+        items: [
+          { text: 'Profiles', link: '/profiles' },
+          { text: 'Routed Service', link: '/routed' }
+        ]
+      },
+      {
+        text: 'Testing',
+        items: [
+          { text: 'Cucumber Tests', link: '/cucumber' }
+        ]
+      },
+      {
+        text: 'Platform Specific',
+        items: [
+          { text: 'Windows Dependencies', link: '/windows-deps' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Project-OSRM/osrm-backend' }
+    ],
+
+    search: {
+      provider: 'local'
+    },
+
+    editLink: {
+      pattern: 'https://github.com/Project-OSRM/osrm-backend/edit/master/docs/:path'
+    }
+  },
+
+  markdown: {
+    theme: {
+      light: 'github-light',
+      dark: 'github-dark'
+    },
+    config: (md) => {
+      md.use(endpointPlugin)
+    }
+  }
+})
