27a4419f48671c6c4e5bbfe77d149b3326c73cee
Marcello Perathoner <marcello@perathoner.de>
2026-04-02 17:33:05 +0200

    Replace XORFastHash by ANDHash (#7400)

--- include/contractor/contractor_heap.hpp
+++ include/contractor/contractor_heap.hpp
@@ -1,9 +1,9 @@
 #ifndef OSRM_CONTRACTOR_CONTRACTOR_HEAP_HPP_
 #define OSRM_CONTRACTOR_CONTRACTOR_HEAP_HPP_
 
+#include "util/linear_hash_storage.hpp"
 #include "util/query_heap.hpp"
 #include "util/typedefs.hpp"
-#include "util/xor_fast_hash_storage.hpp"
 
 namespace osrm::contractor
 {
@@ -20,7 +20,7 @@ using ContractorHeap = util::QueryHeap<NodeID,
                                        NodeID,
                                        EdgeWeight,
                                        ContractorHeapData,
-                                       util::XORFastHashStorage<NodeID, NodeID>>;
+                                       util::LinearHashStorage<NodeID, NodeID>>;
 
 } // namespace osrm::contractor
 
