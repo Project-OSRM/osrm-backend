c6322c6f5f04d3b27d74c155574a34351f5067b9
Rahul Kumar <rahulkumar@Mac.lan>
2026-04-04 20:14:55 +0530

    Format route annotation headers

--- include/extractor/node_based_edge.hpp
+++ include/extractor/node_based_edge.hpp
@@ -84,19 +84,17 @@ struct NodeBasedEdgeAnnotation
 
     bool operator<(const NodeBasedEdgeAnnotation &other) const
     {
-        return (
-            std::tie(string_view_id,
-                     lane_description_id,
-                     classes,
-                     way_id,
-                     travel_mode,
-                     is_left_hand_driving) <
-            std::tie(other.string_view_id,
-                     other.lane_description_id,
-                     other.classes,
-                     other.way_id,
-                     other.travel_mode,
-                     other.is_left_hand_driving));
+        return (std::tie(string_view_id,
+                         lane_description_id,
+                         classes,
+                         way_id,
+                         travel_mode,
+                         is_left_hand_driving) < std::tie(other.string_view_id,
+                                                          other.lane_description_id,
+                                                          other.classes,
+                                                          other.way_id,
+                                                          other.travel_mode,
+                                                          other.is_left_hand_driving));
     }
 };
 
