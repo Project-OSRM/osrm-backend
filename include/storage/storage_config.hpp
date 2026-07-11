a19f100c373380e4235b519286821f5a464e5976
Dennis Luxen <dennis@project-osrm.org>
2026-07-11 11:52:35 +0200

    fix: formatting

--- include/storage/storage_config.hpp
+++ include/storage/storage_config.hpp
@@ -103,17 +103,16 @@ struct StorageConfig final : IOConfig
     }
 
     StorageConfig(const std::vector<storage::FeatureDataset> &disabled_feature_datasets_ = {})
-        : IOConfig(
-              GetRequiredFiles(disabled_feature_datasets_),
-              {".osrm.hsgr",
-               ".osrm.cells",
-               ".osrm.cell_metrics",
-               ".osrm.mldgr",
-               ".osrm.partition",
-               ".osrm.openareas",
-               ".osrm.openareas.ramIndex",
-               ".osrm.openareas.fileIndex"},
-              {})
+        : IOConfig(GetRequiredFiles(disabled_feature_datasets_),
+                   {".osrm.hsgr",
+                    ".osrm.cells",
+                    ".osrm.cell_metrics",
+                    ".osrm.mldgr",
+                    ".osrm.partition",
+                    ".osrm.openareas",
+                    ".osrm.openareas.ramIndex",
+                    ".osrm.openareas.fileIndex"},
+                   {})
     {
     }
 };
